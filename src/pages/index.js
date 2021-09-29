18fac069d905d4534a513210f22562565e538e9c
Szymon Rojek <sz.rojek@gmail.com>
2021-09-29 17:11:47 +0100

    fix exports

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
 export { default as Subscribers } from "./Subscribers";
-export { default as AddSubscriber } from "./Addubscriber";
+export { default as AddSubscriber } from "./AddSubscriber";
 export { default as ListCampaigns } from "./ListCampaigns";
 export { default as CreateCampaign } from "./CreateCampaign";
