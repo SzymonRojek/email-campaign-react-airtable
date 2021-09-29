8798a025c72389987047091346c0bc404d7b995a
Szymon Rojek <sz.rojek@gmail.com>
2021-09-29 17:12:25 +0100

    edit code general cleaning

--- src/pages/Addubscriber.js
+++
@@ -1,3 +0,0 @@
-const AddSubscriber = () => <p>Add Subscriber</p>;
-
-export default AddSubscriber;
--- src/pages/Subscribers.js
+++ src/pages/Subscribers.js
@@ -1,3 +1,42 @@
-const Subscribers = () => <p>Subscribers</p>;
+import {
+  BrowserRouter as Switch,
+  Route,
+  Link as RouterLink,
+} from "react-router-dom";
+import { Button } from "@mui/material";
+import { AddSubscriber } from ".";
+
+const Subscribers = () => (
+  <>
+    <Switch>
+      <Route
+        exact
+        path="/add-subscriber"
+        render={() => {
+          return <AddSubscriber />;
+        }}
+      />
+
+      <Route
+        exact
+        path="/"
+        render={() => (
+          <>
+            <p>Subscribers</p>
+
+            <Button
+              component={RouterLink}
+              to="/add-subscriber"
+              variant="contained"
+              color="primary"
+            >
+              Add New Subsribers
+            </Button>
+          </>
+        )}
+      />
+    </Switch>
+  </>
+);
 
 export default Subscribers;
