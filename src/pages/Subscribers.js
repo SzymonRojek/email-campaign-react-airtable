851592df061e2aa5979f490a7e848417e36866ea
Szymon Rojek <sz.rojek@gmail.com>
2021-09-30 18:55:00 +0100

    devide component into a smaller - Subscriber and SubscribersTable

--- src/pages/Subscribers.js
+++ src/pages/Subscribers.js
@@ -3,40 +3,51 @@ import {
   Route,
   Link as RouterLink,
 } from "react-router-dom";
-import { Button } from "@mui/material";
 import { AddSubscriber } from ".";
+import { apiConfig, getSubscribers } from "./../api/api";
+import { useEffect, useState } from "react";
+import { Button } from "@mui/material";
+import { SubscribersTable } from "../components/SubscribersTable";
 
-const Subscribers = () => (
-  <>
-    <Switch>
-      <Route
-        exact
-        path="/add-subscriber"
-        render={() => {
-          return <AddSubscriber />;
-        }}
-      />
+const Subscribers = () => {
+  const [dataSubscribers, setDataSubscribers] = useState([]);
+  const { subscribers } = apiConfig;
 
-      <Route
-        exact
-        path="/"
-        render={() => (
-          <>
-            <p>Subscribers</p>
+  const getData = async () => {
+    const data = await getSubscribers(subscribers);
+    setDataSubscribers(data);
+  };
 
-            <Button
-              component={RouterLink}
-              to="/add-subscriber"
-              variant="contained"
-              color="primary"
-            >
-              Add New Subsribers
-            </Button>
-          </>
-        )}
-      />
-    </Switch>
-  </>
-);
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <>
+      <Switch>
+        <Route exact path="/add-subscriber" render={() => <AddSubscriber />} />
+
+        <Route
+          exact
+          path="/"
+          render={() => (
+            <>
+              <SubscribersTable dataSubscribers={dataSubscribers} />
+
+              <Button
+                component={RouterLink}
+                to="/add-subscriber"
+                variant="contained"
+                color="secondary"
+              >
+                Add New Subsribers
+              </Button>
+            </>
+          )}
+        />
+      </Switch>
+    </>
+  );
+};
 
 export default Subscribers;
