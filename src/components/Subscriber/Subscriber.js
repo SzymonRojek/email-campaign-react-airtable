cad689a36f7c72b9778b89a12b68f22f6d6f9ea9
Szymon Rojek <sz.rojek@gmail.com>
2021-10-02 10:36:12 +0100

    edit file

--- src/components/Subscriber/Subscriber.js
+++ src/components/Subscriber/Subscriber.js
@@ -1,67 +1,69 @@
-import moment from "moment";
+import {
+  TableCell,
+  TableRow,
+  Avatar,
+  Grid,
+  Typography,
+} from "@material-ui/core";
 
-import { TableCell, TableRow, Avatar, Grid } from "@material-ui/core";
+import { isEven, getFormattedData, getStatusColor } from "./../../helpers";
 
-const isEven = (index) => (index % 2 ? "#F5F5F5" : "");
-
-const getFormattedData = (data) => {
-  const time = data.slice(0, 10);
-  return moment(time).format("YYYY/MM/DD");
-};
-
-const styledStatus = {
-  active: {
-    color: "white",
-    backgroundColor: "#6aec6a",
-    padding: 5,
-    borderRadius: 2,
-  },
-  deactive: {
-    color: "white",
-    backgroundColor: "#ec4f4f",
-    padding: 5,
-    borderRadius: 2,
-  },
-};
-
-const Subscriber = ({ person, index, classes }) => {
-  return (
-    <TableRow
-      key={`key-${index}`}
-      sx={{
-        "&:last-child td, &:last-child th": { border: 0 },
-      }}
-      style={{ backgroundColor: isEven(index) }}
-      className={classes.hover}
-    >
-      <TableCell component="th" scope="row">
-        {++index}.
-      </TableCell>
-      <TableCell>
-        <Grid container alignItems="center">
-          <Grid item sm={4}>
-            <Avatar alt={`Image of ${person.fields.name}'s`}>
-              {person.fields.name[0]}
-            </Avatar>
-          </Grid>
-          <Grid item>{person.fields.name}</Grid>
+const Subscriber = ({ person, index, classes }) => (
+  <TableRow
+    key={`id-${person.id}`}
+    sx={{
+      "&:last-child td, &:last-child th": { border: 0 },
+    }}
+    style={{ backgroundColor: isEven(index) }}
+    className={classes.hover}
+  >
+    <TableCell component="th" scope="row">
+      {++index}.
+    </TableCell>
+    <TableCell>
+      <Grid container alignItems="center">
+        <Grid item sm={4}>
+          <Avatar
+            alt={`Image of ${person.fields.name}'s`}
+            style={{ backgroundColor: getStatusColor(person.fields.status) }}
+          >
+            {person.fields.name[0]}
+          </Avatar>
         </Grid>
-      </TableCell>
-      <TableCell>{person.fields.surname}</TableCell>
-      <TableCell>{person.fields.email}</TableCell>
-      <TableCell>
-        <span
-          style={
-            person.fields.status ? styledStatus.active : styledStatus.deactive
-          }
+        <Grid item>
+          <Typography color="textSecondary" variant="body2">
+            {person.fields.name}
+          </Typography>
+        </Grid>
+      </Grid>
+    </TableCell>
+    <TableCell>
+      <Typography color="textSecondary" variant="body2">
+        {person.fields.surname}
+      </Typography>
+    </TableCell>
+    <TableCell>
+      <Typography color="textSecondary" variant="body2">
+        {person.fields.email}
+      </Typography>
+    </TableCell>
+    <TableCell>
+      <span>
+        <Typography
+          key={index}
+          className={classes.status}
+          style={{ backgroundColor: getStatusColor(person.fields.status) }}
         >
-          {" "}
-          {person.fields.status ? "active" : "deactive"}
-        </span>
-      </TableCell>
-      <TableCell>{getFormattedData(person.createdTime)}</TableCell>
-    </TableRow>
-  );
-};
+          {person.fields.status}
+        </Typography>
+      </span>
+    </TableCell>
+    <TableCell>
+      <Typography color="textSecondary" variant="body2">
+        {getFormattedData(person.createdTime)}
+      </Typography>
+    </TableCell>
+  </TableRow>
+);
 
 export default Subscriber;
