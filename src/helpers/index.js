0b19c710d921ff45ca25869c7b98f66532108bb3
Szymon Rojek <sz.rojek@gmail.com>
2021-10-02 10:34:01 +0100

    create index.js for exporting helpers

---
+++ src/helpers/index.js
@@ -0,0 +1,4 @@
+export { default as getStatusColor } from "./getStatusColor";
+export { default as getFormattedData } from "./getFormattedData";
+export { default as isEven } from "./isEven";
+export { default as validationSchema } from "./validationSchema";
