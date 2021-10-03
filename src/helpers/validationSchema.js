752164eef108bed79c2ff61743de65824e7743e9
Szymon Rojek <sz.rojek@gmail.com>
2021-10-03 23:45:29 +0100

    edit validationScheme

--- src/helpers/validationSchema.js
+++ src/helpers/validationSchema.js
@@ -9,8 +9,9 @@ const validationSchema = Yup.object().shape({
   surname: Yup.string()
     .required("Surname is required")
     .matches(/^[aA-zZ\s]+$/, "Only letters are required")
-    .min(3, "Surname must be at least 6 characters")
+    .min(3, "Surname must be at least 3 characters")
     .max(10, "Surname must not exceed 10 characters"),
   email: Yup.string().required("Email is required").email("Email is invalid"),
 });
+
 export default validationSchema;
