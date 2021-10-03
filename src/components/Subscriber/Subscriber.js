c3e8a92aeae0e2f70eaf55ca7dd97d20643acc25
Szymon Rojek <sz.rojek@gmail.com>
2021-10-03 19:51:33 +0100

    add useHistory hook, onClick to the TableRow, setPopup text function and setOpenPopup when clicked,

--- src/components/Subscriber/Subscriber.js
+++ src/components/Subscriber/Subscriber.js
@@ -1,3 +1,4 @@
+import { useHistory } from "react-router-dom";
 import {
   TableCell,
   TableRow,
@@ -6,64 +7,87 @@ import {
   Typography,
 } from "@material-ui/core";
 
-import { isEven, getFormattedData, getStatusColor } from "./../../helpers";
+import {
+  isEven,
+  getFormattedData,
+  getStatusColor,
+  setTextPopup,
+  isStatusActive,
+} from "./../../helpers";
 
-const Subscriber = ({ person, index, classes }) => (
-  <TableRow
-    key={`id-${person.id}`}
-    sx={{
-      "&:last-child td, &:last-child th": { border: 0 },
-    }}
-    style={{ backgroundColor: isEven(index) }}
-    className={classes.hover}
-  >
-    <TableCell component="th" scope="row">
-      {++index}.
-    </TableCell>
-    <TableCell>
-      <Grid container alignItems="center">
-        <Grid item sm={4}>
-          <Avatar
-            alt={`Image of ${person.fields.name}'s`}
+const Subscriber = (props) => {
+  let { person, index, classes, setOpenPopup, setTextContentPopup } = props;
+  const history = useHistory();
+  const handleRowClick = () => history.push(`/subscribers/${person.id}`);
+
+  return (
+    <TableRow
+      onClick={() => {
+        setTextPopup(
+          person.fields.status,
+          person.fields.name,
+          setTextContentPopup
+        );
+        setOpenPopup(() =>
+          isStatusActive(person.fields.status, handleRowClick)
+        );
+      }}
+      key={`id-${person.id}`}
+      sx={{
+        "&:last-child td, &:last-child th": { border: 0 },
+      }}
+      style={{ backgroundColor: isEven(index) }}
+    >
+      <TableCell component="th" scope="row">
+        {++index}.
+      </TableCell>
+      <TableCell>
+        <Grid container alignItems="center">
+          <Grid item sm={4}>
+            <Avatar
+              alt={`Image of ${person.fields.name}'s`}
+              style={{
+                backgroundColor: getStatusColor(person.fields.status),
+              }}
+            >
+              {person.fields.name[0]}
+            </Avatar>
+          </Grid>
+          <Grid item>
+            <Typography color="textSecondary" variant="body2">
+              {person.fields.name}
+            </Typography>
+          </Grid>
+        </Grid>
+      </TableCell>
+      <TableCell>
+        <Typography color="textSecondary" variant="body2">
+          {person.fields.surname}
+        </Typography>
+      </TableCell>
+      <TableCell>
+        <Typography color="textSecondary" variant="body2">
+          {person.fields.email}
+        </Typography>
+      </TableCell>
+      <TableCell>
+        <span>
+          <Typography
+            key={index}
+            className={classes.status}
             style={{ backgroundColor: getStatusColor(person.fields.status) }}
           >
-            {person.fields.name[0]}
-          </Avatar>
-        </Grid>
-        <Grid item>
-          <Typography color="textSecondary" variant="body2">
-            {person.fields.name}
+            {person.fields.status}
           </Typography>
-        </Grid>
-      </Grid>
-    </TableCell>
-    <TableCell>
-      <Typography color="textSecondary" variant="body2">
-        {person.fields.surname}
-      </Typography>
-    </TableCell>
-    <TableCell>
-      <Typography color="textSecondary" variant="body2">
-        {person.fields.email}
-      </Typography>
-    </TableCell>
-    <TableCell>
-      <span>
-        <Typography
-          key={index}
-          className={classes.status}
-          style={{ backgroundColor: getStatusColor(person.fields.status) }}
-        >
-          {person.fields.status}
+        </span>
+      </TableCell>
+      <TableCell>
+        <Typography color="textSecondary" variant="body2">
+          {getFormattedData(person.createdTime)}
         </Typography>
-      </span>
-    </TableCell>
-    <TableCell>
-      <Typography color="textSecondary" variant="body2">
-        {getFormattedData(person.createdTime)}
-      </Typography>
-    </TableCell>
-  </TableRow>
-);
+      </TableCell>
+    </TableRow>
+  );
+};
 
 export default Subscriber;
