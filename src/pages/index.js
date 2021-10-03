bbeb06f64e364d7eacf6f9266374de0260e54f99
Szymon Rojek <sz.rojek@gmail.com>
2021-10-03 23:45:54 +0100

    add Home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,3 +2,4 @@ export { default as Subscribers } from "./Subscribers";
 export { default as AddSubscriber } from "./AddSubscriber";
 export { default as ListCampaigns } from "./ListCampaigns";
 export { default as CreateCampaign } from "./CreateCampaign";
+export { default as Home } from "./Home";
