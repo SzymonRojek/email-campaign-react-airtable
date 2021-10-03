c3524ee86fd9c50b9b3fc11f7280b8b928ff4fa4
Szymon Rojek <sz.rojek@gmail.com>
2021-10-03 23:45:09 +0100

    remove isStatusActive, add margin top and bottom, auto

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -3,4 +3,3 @@ export { default as getFormattedData } from "./getFormattedData";
 export { default as isEven } from "./isEven";
 export { default as validationSchema } from "./validationSchema";
 export { default as setTextPopup } from "./setTextPopup";
-export { default as isStatusActive } from "./isStatusActive";
