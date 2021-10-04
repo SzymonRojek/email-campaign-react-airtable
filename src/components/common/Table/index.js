039086cf8d40159cf075bd39437fef95a3f46855
Szymon Rojek <sz.rojek@gmail.com>
2021-10-04 13:42:23 +0100

    add exports for the common dir

---
+++ src/components/common/Table/index.js
@@ -0,0 +1,4 @@
+export { default as ContainerTable } from "./ContainerTable";
+export { default as HeadTable } from "./HeadTable";
+export { default as BodyTable } from "./BodyTable";
+export { default as RowSubscriber } from "./RowSubscriber";
