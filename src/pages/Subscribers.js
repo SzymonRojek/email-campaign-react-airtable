ba86c4888e91a4edb2ed9b08a01c8e54a737ab57
Szymon Rojek <sz.rojek@gmail.com>
2021-10-04 13:47:01 +0100

    rename dataSubscribers to subscribersData, clean code

--- src/pages/Subscribers.js
+++ src/pages/Subscribers.js
@@ -4,17 +4,16 @@ import {
   Route,
   Link as RouterLink,
 } from "react-router-dom";
-
 import { Button, Box } from "@mui/material";
-import AddSubscriber from "./AddSubscriber";
 
+import api from "./../api";
+import AddSubscriber from "./AddSubscriber";
+import SubscriberDetails from "./SubscriberDetails";
 import { SubscribersList } from "../components/SubscribersList";
 import { Popup } from "../components/Popup";
-import SubscriberDetails from "./SubscriberDetails";
-import api from "./../api";
 
 const Subscribers = () => {
-  const [dataSubscribers, setDataSubscribers] = useState([]);
+  const [subscribersData, setsubscribersData] = useState([]);
   const [openPopup, setOpenPopup] = useState(false);
   const [textContentPopup, setContentPopup] = useState("");
 
@@ -22,7 +21,7 @@ const Subscribers = () => {
 
   const getData = async () => {
     const data = await api.get(endpoint);
-    setDataSubscribers(data.records);
+    setsubscribersData(data.records);
   };
 
   useEffect(() => {
@@ -42,7 +41,7 @@ const Subscribers = () => {
 
         <Route exact path="/subscribers">
           <SubscribersList
-            dataSubscribers={dataSubscribers}
+            subscribersData={subscribersData}
             openPopup={openPopup}
             setOpenPopup={setOpenPopup}
             setContentPopup={setContentPopup}
@@ -60,7 +59,6 @@ const Subscribers = () => {
           </Box>
         </Route>
       </Switch>
-
       <Popup
         openPopup={openPopup}
         setOpenPopup={setOpenPopup}
