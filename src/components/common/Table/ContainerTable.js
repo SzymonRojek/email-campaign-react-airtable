4a1fbe24be6114e6ce0ea76077f5082c8c33859c
Szymon Rojek <sz.rojek@gmail.com>
2021-10-06 13:46:11 +0100

    change max-width of the container, and add elevation box shadow

--- src/components/common/Table/ContainerTable.js
+++ src/components/common/Table/ContainerTable.js
@@ -5,14 +5,18 @@ const useStyles = makeStyles((theme) => ({
   tableContainer: {
     borderRadius: 15,
     margin: "100px auto",
-    maxWidth: 950,
+    maxWidth: 1200,
   },
 }));
 
 const ContainerTable = ({ children }) => {
   const classes = useStyles();
   return (
-    <TableContainer component={Paper} className={classes.tableContainer}>
+    <TableContainer
+      component={Paper}
+      className={classes.tableContainer}
+      elevation={4}
+    >
       <Table aria-label="subscribers table">{children}</Table>
     </TableContainer>
   );
