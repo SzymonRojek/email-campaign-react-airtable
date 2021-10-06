1f3f7c34ccf8f23e181e5a5a86b8bebd72fa7c61
Szymon Rojek <sz.rojek@gmail.com>
2021-10-06 16:49:08 +0100

    change imports

--- src/components/SubscribersList/FilteredStatusSubscribers.js
+++ src/components/SubscribersList/FilteredStatusSubscribers.js
@@ -1,7 +1,7 @@
 import { TableCell } from "@material-ui/core";
 
-import { ContainerTable, HeadTable, BodyTable } from "../common/Table";
-import { RowSubscriber } from "../common/Table";
+import { ContainerTable, HeadTable, BodyTable } from "../../common/Table";
+import { RowSubscriber } from "../../common/Table";
 
 const FilteredStatusSubscribers = (props) => {
   const {
