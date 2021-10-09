53ad0cc66833d90779c540cdbd723c8870bf1d85
Szymon Rojek <sz.rojek@gmail.com>
2021-10-09 14:36:29 +0100

    change imports, add invoke getData function from api when new subscriber is added

--- src/pages/AddSubscriber.js
+++ src/pages/AddSubscriber.js
@@ -3,11 +3,11 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import { Paper, Button, Box } from "@mui/material";
 import { Grid, Typography } from "@material-ui/core";
 
-import api from "./../api";
-import { TextInput } from "./../components/TextInput";
-import { capitalizeFirstLetter, validationSchema } from "./../helpers";
+import api from "../api";
+import { TextInput } from "../components/TextInput";
+import { capitalizeFirstLetter, validationSchema } from "../helpers";
 
-const AddSubscriber = ({ setOpenInfoPopup, setContentPopup }) => {
+const AddSubscriber = ({ setOpenInfoPopup, setContentPopup, getData }) => {
   const {
     register,
     handleSubmit,
@@ -35,6 +35,8 @@ const AddSubscriber = ({ setOpenInfoPopup, setContentPopup }) => {
         data.name
       )} has been added to the data :D`,
     });
+
+    getData();
     setOpenInfoPopup(true);
 
     setTimeout(() => {
@@ -43,61 +45,63 @@ const AddSubscriber = ({ setOpenInfoPopup, setContentPopup }) => {
   };
 
   return (
-    <Box display="flex" justifyContent="center" alignItems="center">
-      <Paper
-        elevation={6}
-        style={{
-          maxWidth: 500,
-          marginTop: 100,
-          textAlign: "center",
-          borderRadius: 8,
-        }}
-      >
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <Box px={3} py={4}>
-            <Grid container spacing={1}>
-              <Typography color="textSecondary" variant="body2">
-                *Field required
-              </Typography>
+    <>
+      <Box display="flex" justifyContent="center" alignItems="center">
+        <Paper
+          elevation={6}
+          style={{
+            maxWidth: 500,
+            marginTop: 100,
+            textAlign: "center",
+            borderRadius: 8,
+          }}
+        >
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Box px={3} py={4}>
+              <Grid container spacing={1}>
+                <Typography color="textSecondary" variant="body2">
+                  *Field required
+                </Typography>
 
-              <TextInput
-                value="name"
-                register={register}
-                error={!!errors?.name}
-                message={errors.name?.message ?? ""}
-              />
+                <TextInput
+                  value="name"
+                  register={register}
+                  error={!!errors?.name}
+                  message={errors.name?.message ?? ""}
+                />
 
-              <TextInput
-                value="surname"
-                register={register}
-                error={!!errors?.surname}
-                message={errors.surname?.message ?? ""}
-              />
+                <TextInput
+                  value="surname"
+                  register={register}
+                  error={!!errors?.surname}
+                  message={errors.surname?.message ?? ""}
+                />
 
-              <TextInput
-                value="profession"
-                register={register}
-                error={!!errors?.profession}
-                message={errors.profession?.message ?? ""}
-              />
+                <TextInput
+                  value="profession"
+                  register={register}
+                  error={!!errors?.profession}
+                  message={errors.profession?.message ?? ""}
+                />
 
-              <TextInput
-                value="email"
-                register={register}
-                error={!!errors?.email}
-                message={errors.email?.message ?? ""}
-              />
-            </Grid>
-          </Box>
+                <TextInput
+                  value="email"
+                  register={register}
+                  error={!!errors?.email}
+                  message={errors.email?.message ?? ""}
+                />
+              </Grid>
+            </Box>
 
-          <Box mt={3}>
-            <Button variant="contained" color="primary" type="submit">
-              Add Subscriber
-            </Button>
-          </Box>
-        </form>
-      </Paper>
-    </Box>
+            <Box mt={3}>
+              <Button variant="contained" color="primary" type="submit">
+                Add Subscriber
+              </Button>
+            </Box>
+          </form>
+        </Paper>
+      </Box>
+    </>
   );
 };
 
