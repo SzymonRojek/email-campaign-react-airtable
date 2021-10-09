0a0559e0a1cc27810fb085dd89d53e98304e5b99
Szymon Rojek <sz.rojek@gmail.com>
2021-10-09 14:28:13 +0100

    rearrange aaplication - change navigation to v6 routing, take all data from subscribersList and tranfer it to the App component, also popups

--- src/App.js
+++ src/App.js
@@ -1,22 +1,143 @@
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { useEffect, useState } from "react";
+import {
+  BrowserRouter as Router,
+  useRoutes,
+  useNavigate,
+} from "react-router-dom";
 
-import { Navigation } from "./components/Navigation";
-import { Subscribers, CreateCampaign, ListCampaigns, Home } from "./pages";
+import {
+  SubscribersList,
+  CreateCampaign,
+  ListCampaigns,
+  Home,
+  AddSubscriber,
+  FilteredStatusSubscribers,
+} from "./pages";
+import { MainNavigation, SubscribersNav } from "./components/Navigation";
+import { InfoPopup, ConfirmPopup } from "./components/Popup";
+import api from "./api";
+import {
+  sortDataAlphabetically,
+  capitalizeFirstLetter,
+  setTextPopup,
+} from "./helpers";
+import handlers from "./helpers/handlers";
+
+const Routing = () => {
+  const [subscribersData, setSubscribersData] = useState([]);
+  const [openInfoPopup, setOpenInfoPopup] = useState(false);
+  const [textContentPopup, setContentPopup] = useState({});
+  const [openConfirmPopup, setOpenConfirmPopup] = useState(false);
+  const [idClickedSubscriber, setIdClickedSubscriber] = useState("");
+  const navigate = useNavigate();
+
+  const endpoint = "/subscribers";
+
+  const getData = async () => {
+    const data = await api.get(endpoint);
+    sortDataAlphabetically(data.records);
+    setSubscribersData(data.records);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const handleRemoveSubscriber = async () => {
+    const idSubscriber = subscribersData.filter(
+      (subscriber) => subscriber.id === idClickedSubscriber
+    )[0].id;
+
+    await api.delete(`/subscribers/${idSubscriber}`);
+    getData();
+
+    setOpenConfirmPopup(false);
+  };
+
+  const handlePopup = (subscriber) => {
+    handlers.handleRowClick(subscriber, navigate);
+    setTextPopup(
+      subscriber.fields.status,
+      capitalizeFirstLetter(subscriber.fields.name),
+      setContentPopup
+    );
+    handlers.handleOpenPopup(subscriber, setOpenInfoPopup);
+  };
+
+  const routes = [
+    {
+      path: "/subscribers",
+      element: <SubscribersNav />,
+      children: [
+        {
+          path: "/",
+          element: (
+            <SubscribersList
+              subscribersData={subscribersData}
+              setOpenConfirmPopup={setOpenConfirmPopup}
+              handlePopup={handlePopup}
+              setIdClickedSubscriber={setIdClickedSubscriber}
+            />
+          ),
+        },
+        {
+          path: "/add-subscriber",
+          element: (
+            <AddSubscriber
+              setOpenInfoPopup={setOpenInfoPopup}
+              setContentPopup={setContentPopup}
+              getData={getData}
+            />
+          ),
+        },
+        {
+          path: "/filtered-status",
+          element: (
+            <FilteredStatusSubscribers
+              subscribersData={subscribersData}
+              setContentPopup={setContentPopup}
+              setOpenInfoPopup={setOpenInfoPopup}
+              setOpenConfirmPopup={setOpenConfirmPopup}
+              setIdClickedSubscriber={setIdClickedSubscriber}
+              handlePopup={handlePopup}
+            />
+          ),
+        },
+      ],
+    },
+    { path: "/campaigns", element: <ListCampaigns /> },
+    { path: "/new-campaign", element: <CreateCampaign /> },
+    { path: "/home", element: <Home /> },
+  ];
+
+  const routing = useRoutes(routes);
+
+  return (
+    <>
+      <MainNavigation />
+
+      <InfoPopup
+        openInfoPopup={openInfoPopup}
+        setOpenInfoPopup={setOpenInfoPopup}
+        textContentPopup={textContentPopup}
+      />
+
+      <ConfirmPopup
+        openConfirmPopup={openConfirmPopup}
+        setOpenConfirmPopup={setOpenConfirmPopup}
+        idClickedSubscriber={idClickedSubscriber}
+        setIdClickedSubscriber={setIdClickedSubscriber}
+        removeSubscriber={handleRemoveSubscriber}
+      />
+      <div>{routing}</div>
+    </>
+  );
+};
 
 const App = () => (
-  <BrowserRouter>
-    <Navigation />
-
-    <Switch>
-      <Route exact path="/campaigns" component={ListCampaigns} />
-
-      <Route exact path="/new-campaign" component={CreateCampaign} />
-
-      <Route exact path="/subscribers" component={Subscribers} />
-
-      <Route exact path="/" component={Home} />
-    </Switch>
-  </BrowserRouter>
+  <Router>
+    <Routing />
+  </Router>
 );
 
 export default App;
