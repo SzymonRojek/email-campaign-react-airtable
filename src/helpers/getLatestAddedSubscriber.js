ac4a75f9507044e8d1e6e5fd7fe20576577cbf19
Szymon Rojek <sz.rojek@gmail.com>
2021-10-10 23:35:45 +0100

    modify function getLatestAddedSubscriberData

--- src/helpers/getLatestAddedSubscriber.js
+++ src/helpers/getLatestAddedSubscriber.js
@@ -1,18 +1,4 @@
-import moment from "moment";
-
-const formattedDate = (date) => moment(date).format("YYYY-MM-DD HH:mm:ss");
-
-const getLatestAddedSubscriber = (data) => {
-  data = [...data];
-
-  const sortedData = data.sort((a, b) => {
-    const dateA = new Date(formattedDate(a.createdTime)).getTime();
-    const dateB = new Date(formattedDate(b.createdTime)).getTime();
-
-    return dateA > dateB ? 1 : -1;
-  });
-
-  return sortedData.slice(-1);
-};
+const getLatestAddedSubscriber = (data) =>
+  [...data].sort((a, b) => (a.createdTime > b.createdTime ? 1 : -1)).slice(-1);
 
 export default getLatestAddedSubscriber;
