8da9e8396b4b8337d8ff086955c4b5d9acc8162e
Szymon Rojek <sz.rojek@gmail.com>
2021-10-11 22:06:16 +0100

    add Loader to the SubscribersList

--- src/pages/pagesApp/SubscribersList.js
+++ src/pages/pagesApp/SubscribersList.js
@@ -1,4 +1,5 @@
 import { ContainerTableSubscribers } from "../../components/ContainerTableSubscribers";
+import { Loader } from "../../components/Loader";
 import { generalDataHeadTable } from "../../data/dataHeadTable";
 
 const SubscribersList = (props) => {
@@ -7,30 +8,35 @@ const SubscribersList = (props) => {
     setOpenConfirmPopup,
     handlePopup,
     setIdClickedSubscriber,
-    latestAddedSubscriber,
   } = props;
-
+  console.log(subscribersData);
   return (
     <>
-      <ContainerTableSubscribers
-        dataHeadTable={generalDataHeadTable}
-        subscribersData={subscribersData}
-        setOpenConfirmPopup={setOpenConfirmPopup}
-        handlePopup={handlePopup}
-        setIdClickedSubscriber={setIdClickedSubscriber}
-      />
-
-      <h2 style={{ textAlign: "center", marginBottom: -40, color: "#303f9f" }}>
-        Latest added subscriber:
-      </h2>
-
-      <ContainerTableSubscribers
-        dataHeadTable={generalDataHeadTable}
-        subscribersData={latestAddedSubscriber}
-        setOpenConfirmPopup={setOpenConfirmPopup}
-        handlePopup={handlePopup}
-        setIdClickedSubscriber={setIdClickedSubscriber}
-      />
+      {subscribersData.status === "loading" ? (
+        <Loader title="Subscribers Data are loading..." />
+      ) : (
+        <>
+          <ContainerTableSubscribers
+            dataHeadTable={generalDataHeadTable}
+            subscribersData={subscribersData.data}
+            setOpenConfirmPopup={setOpenConfirmPopup}
+            handlePopup={handlePopup}
+            setIdClickedSubscriber={setIdClickedSubscriber}
+          />
+          <h2
+            style={{ textAlign: "center", marginBottom: -40, color: "#303f9f" }}
+          >
+            Latest added subscriber:
+          </h2>
+          <ContainerTableSubscribers
+            dataHeadTable={generalDataHeadTable}
+            subscribersData={subscribersData.latestSubscriber}
+            setOpenConfirmPopup={setOpenConfirmPopup}
+            handlePopup={handlePopup}
+            setIdClickedSubscriber={setIdClickedSubscriber}
+          />
+        </>
+      )}
     </>
   );
 };
