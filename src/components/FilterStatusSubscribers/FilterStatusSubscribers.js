3788b05cef551f92b0f44f6c1e17a15e107b0997
Szymon Rojek <sz.rojek@gmail.com>
2021-10-11 22:02:36 +0100

    fix the bug - return empty string if there is no data

--- src/components/FilterStatusSubscribers/FilterStatusSubscribers.js
+++ src/components/FilterStatusSubscribers/FilterStatusSubscribers.js
@@ -14,35 +14,35 @@ const FilterStatusSubscribers = (props) => {
     removeSubscriber,
   } = props;
 
-  return (
-    subscribersData &&
-    subscribersData.some((el) => el.fields.status === status) && (
-      <ContainerTable>
-        <HeadTable dataHeadTable={generalDataHeadTable} />
+  return subscribersData &&
+    subscribersData.some((el) => el.fields.status === status) ? (
+    <ContainerTable>
+      <HeadTable dataHeadTable={generalDataHeadTable} />
 
-        <BodyTable>
-          {subscribersData &&
-            subscribersData
-              .filter((subscriber) => subscriber.fields.status === status)
-              .map((subscriber, index) => (
-                <RowSubscriber
-                  children={
-                    <TableCell component="th" scope="row">
-                      {++index}.
-                    </TableCell>
-                  }
-                  key={`id-${subscriber.id}`}
-                  subscriber={subscriber}
-                  index={index}
-                  handlePopup={handlePopup}
-                  setOpenConfirmPopup={setOpenConfirmPopup}
-                  setIdClickedSubscriber={setIdClickedSubscriber}
-                  removeSubscriber={removeSubscriber}
-                />
-              ))}
-        </BodyTable>
-      </ContainerTable>
-    )
+      <BodyTable>
+        {subscribersData &&
+          subscribersData
+            .filter((subscriber) => subscriber.fields.status === status)
+            .map((subscriber, index) => (
+              <RowSubscriber
+                children={
+                  <TableCell component="th" scope="row">
+                    {++index}.
+                  </TableCell>
+                }
+                key={`id-${subscriber.id}`}
+                subscriber={subscriber}
+                index={index}
+                handlePopup={handlePopup}
+                setOpenConfirmPopup={setOpenConfirmPopup}
+                setIdClickedSubscriber={setIdClickedSubscriber}
+                removeSubscriber={removeSubscriber}
+              />
+            ))}
+      </BodyTable>
+    </ContainerTable>
+  ) : (
+    ""
   );
 };
 
