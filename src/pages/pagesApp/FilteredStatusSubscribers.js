b28045882e5574b514d1bdf2cbfc166037a82214
Szymon Rojek <sz.rojek@gmail.com>
2021-10-14 17:56:45 +0200

    add ternary operator when there are no subscribers in the tablefd

--- src/pages/pagesApp/FilteredStatusSubscribers.js
+++ src/pages/pagesApp/FilteredStatusSubscribers.js
@@ -14,40 +14,48 @@ const FilteredStatusSubscribers = (props) => {
   return (
     <>
       {subscribersData.status === "loading" ? (
-        <Loader title="Data are filtering by status" />
+        <Loader title="Subscribers Data List is loading..." />
       ) : subscribersData.status === "success" ? (
         <>
-          <FilterStatusSubscribers
-            generalDataHeadTable={generalDataHeadTable}
-            subscribersData={subscribersData.data}
-            status="active"
-            setOpenConfirmPopup={setOpenConfirmPopup}
-            setIdClickedSubscriber={setIdClickedSubscriber}
-            handlePopup={handlePopup}
-          />
-
-          <FilterStatusSubscribers
-            generalDataHeadTable={generalDataHeadTable}
-            subscribersData={subscribersData.data}
-            status="pending"
-            setOpenConfirmPopup={setOpenConfirmPopup}
-            setIdClickedSubscriber={setIdClickedSubscriber}
-            handlePopup={handlePopup}
-          />
-
-          <FilterStatusSubscribers
-            generalDataHeadTable={generalDataHeadTable}
-            subscribersData={subscribersData.data}
-            status="blocked"
-            setOpenConfirmPopup={setOpenConfirmPopup}
-            setIdClickedSubscriber={setIdClickedSubscriber}
-            handlePopup={handlePopup}
-          />
+          {subscribersData.status === "success" &&
+          !subscribersData.data.length ? (
+            <Error
+              titleOne="There are not subscribers added yet."
+              titleTwo="Please add a new subscriber."
+            />
+          ) : (
+            <>
+              <FilterStatusSubscribers
+                generalDataHeadTable={generalDataHeadTable}
+                subscribersData={subscribersData.data}
+                status="active"
+                setOpenConfirmPopup={setOpenConfirmPopup}
+                setIdClickedSubscriber={setIdClickedSubscriber}
+                handlePopup={handlePopup}
+              />
+              <FilterStatusSubscribers
+                generalDataHeadTable={generalDataHeadTable}
+                subscribersData={subscribersData.data}
+                status="pending"
+                setOpenConfirmPopup={setOpenConfirmPopup}
+                setIdClickedSubscriber={setIdClickedSubscriber}
+                handlePopup={handlePopup}
+              />
+              <FilterStatusSubscribers
+                generalDataHeadTable={generalDataHeadTable}
+                subscribersData={subscribersData.data}
+                status="blocked"
+                setOpenConfirmPopup={setOpenConfirmPopup}
+                setIdClickedSubscriber={setIdClickedSubscriber}
+                handlePopup={handlePopup}
+              />
+            </>
+          )}
         </>
       ) : (
         <Error
           titleOne="ERROR MESSAGE"
-          titleTwo="Probably you have got internet connection problem."
+          titleTwo="Probably there is no an access to the internet."
           titleThree="Contact with your support team."
         />
       )}
