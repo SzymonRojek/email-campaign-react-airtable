74eb71ee1fc3cc4ad081808ceb6067837dd5a76d
Szymon Rojek <sz.rojek@gmail.com>
2021-10-14 13:51:41 +0200

    create Error component

---
+++ src/components/Error/Error.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+
+const Error = ({ titleOne, titleTwo, titleThree }) => (
+  <div className="error-container">
+    <div></div>
+    <p className="error-text error-mainText">{titleOne}</p>
+    <p className="error-text error-subText">{titleTwo}</p>
+    <p className="error-text error-subText">{titleThree}</p>
+  </div>
+);
+
+export default Error;
