83ebc838caf09dd453d2b9abbc296a104cde9baf
Szymon Rojek <sz.rojek@gmail.com>
2021-10-14 13:59:17 +0200

    changeDefault value of the subscriberData state variable to the object, add try catch, setTimeout to the useEffect and Loader to the subscriberDetails

--- src/pages/pagesApp/SubscriberDetails.js
+++ src/pages/pagesApp/SubscriberDetails.js
@@ -12,82 +12,98 @@ import {
   detailsDataHeadTableFirst,
   detailsDataHeadTableSecond,
 } from "../../data/dataHeadTable";
+import { Loader } from "../../components/Loader";
 
 const SubscriberDetails = () => {
+  const [subscriberData, setSubscriberData] = useState({
+    status: "loading",
+    data: null,
+  });
   const { id } = useParams();
-  const [subscriberData, setSubscriberData] = useState(null);
-
   const endpoint = `/subscribers/${id}`;
 
-  const getSubscriber = async () => {
-    const data = await api.get(endpoint);
-    setSubscriberData([data]);
+  const getSubscriberData = async () => {
+    try {
+      const data = await api.get(endpoint);
+
+      setSubscriberData({ status: "success", data });
+    } catch (error) {
+      setSubscriberData({ status: "error" });
+    }
   };
 
   useEffect(() => {
-    getSubscriber();
+    const delayGetData = setTimeout(getSubscriberData, 1_000);
+
+    return () => clearTimeout(delayGetData);
   }, []);
 
   return (
     <>
-      <h1
-        style={{
-          textAlign: "center",
-          marginTop: 100,
-          fontSize: 40,
-          color: "#303f9f",
-          letterSpacing: 2,
-          wordSpacing: 15,
-        }}
-      >
-        Subscriber Details
-      </h1>
-      <ContainerTable>
-        <HeadTable dataHeadTable={detailsDataHeadTableFirst} />
+      {subscriberData.status === "loading" ? (
+        <Loader title={`Subscriber's details are loading...`} />
+      ) : (
+        <>
+          <h1
+            style={{
+              textAlign: "center",
+              marginTop: 100,
+              fontSize: 40,
+              color: "#303f9f",
+              letterSpacing: 2,
+              wordSpacing: 15,
+            }}
+          >
+            Subscriber Details
+          </h1>
+          <ContainerTable>
+            <HeadTable dataHeadTable={detailsDataHeadTableFirst} />
 
-        <TableBody>
-          {subscriberData &&
-            subscriberData.map((subscriber, index) => (
-              <RowSubscriber
-                key={`id-${subscriber.id}`}
-                subscriber={subscriber}
-                index={index}
-              />
-            ))}
-        </TableBody>
-      </ContainerTable>
+            <TableBody>
+              {subscriberData &&
+                [subscriberData.data].map((subscriber, index) => (
+                  <RowSubscriber
+                    key={`id-${subscriber.id}`}
+                    subscriber={subscriber}
+                    index={index}
+                  />
+                ))}
+            </TableBody>
+          </ContainerTable>
 
-      <ContainerTable>
-        <HeadTable dataHeadTable={detailsDataHeadTableSecond} />
+          <ContainerTable>
+            <HeadTable dataHeadTable={detailsDataHeadTableSecond} />
 
-        <TableBody>
-          {subscriberData &&
-            subscriberData.map((subscriber, index) => (
-              <TableRow key={`i-${index}`}>
-                <TableCell>
-                  <Typography color="textSecondary" variant="body2">
-                    {subscriber.fields?.email}
-                  </Typography>
-                </TableCell>
-                <TableCell>
-                  <Typography color="textSecondary" variant="body2">
-                    {subscriber.fields?.salary}
-                  </Typography>
-                </TableCell>
-                <TableCell>
-                  <Typography color="textSecondary" variant="body2">
-                    {subscriber.fields?.telephone}
-                  </Typography>
-                </TableCell>
-                <TableCell>
-                  <Typography color="textSecondary" variant="body2">
-                    {subscriber.fields?.address}
-                  </Typography>
-                </TableCell>
-              </TableRow>
-            ))}
-        </TableBody>
-      </ContainerTable>
+            <TableBody>
+              {subscriberData &&
+                [subscriberData.data].map((subscriber, index) => (
+                  <TableRow key={`i-${index}`}>
+                    <TableCell>
+                      <Typography color="textSecondary" variant="body2">
+                        {subscriber.fields?.email}
+                      </Typography>
+                    </TableCell>
+                    <TableCell>
+                      <Typography color="textSecondary" variant="body2">
+                        {subscriber.fields?.salary}
+                      </Typography>
+                    </TableCell>
+                    <TableCell>
+                      <Typography color="textSecondary" variant="body2">
+                        {subscriber.fields?.telephone}
+                      </Typography>
+                    </TableCell>
+                    <TableCell>
+                      <Typography color="textSecondary" variant="body2">
+                        {subscriber.fields?.address}
+                      </Typography>
+                    </TableCell>
+                  </TableRow>
+                ))}
+            </TableBody>
+          </ContainerTable>
+        </>
+      )}
     </>
   );
 };
