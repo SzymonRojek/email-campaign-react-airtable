1014a62b933168b99cad43fba9f0f5ab8f7d265b
Szymon Rojek <sz.rojek@gmail.com>
2021-10-14 13:56:57 +0200

    add Loader and Error with all ternary operators conditions

--- src/pages/pagesApp/FilteredStatusSubscribers.js
+++ src/pages/pagesApp/FilteredStatusSubscribers.js
@@ -1,4 +1,6 @@
 import { FilterStatusSubscribers } from "../../components/FilterStatusSubscribers";
+import { Loader } from "../../components/Loader";
+import { Error } from "../../components/Error";
 import { generalDataHeadTable } from "../../data/dataHeadTable";
 
 const FilteredStatusSubscribers = (props) => {
@@ -11,32 +13,44 @@ const FilteredStatusSubscribers = (props) => {
 
   return (
     <>
-      <FilterStatusSubscribers
-        generalDataHeadTable={generalDataHeadTable}
-        subscribersData={subscribersData}
-        status="active"
-        setOpenConfirmPopup={setOpenConfirmPopup}
-        setIdClickedSubscriber={setIdClickedSubscriber}
-        handlePopup={handlePopup}
-      />
+      {subscribersData.status === "loading" ? (
+        <Loader title="Data are filtering by status" />
+      ) : subscribersData.status === "success" ? (
+        <>
+          <FilterStatusSubscribers
+            generalDataHeadTable={generalDataHeadTable}
+            subscribersData={subscribersData.data}
+            status="active"
+            setOpenConfirmPopup={setOpenConfirmPopup}
+            setIdClickedSubscriber={setIdClickedSubscriber}
+            handlePopup={handlePopup}
+          />
 
-      <FilterStatusSubscribers
-        generalDataHeadTable={generalDataHeadTable}
-        subscribersData={subscribersData}
-        status="pending"
-        setOpenConfirmPopup={setOpenConfirmPopup}
-        setIdClickedSubscriber={setIdClickedSubscriber}
-        handlePopup={handlePopup}
-      />
+          <FilterStatusSubscribers
+            generalDataHeadTable={generalDataHeadTable}
+            subscribersData={subscribersData.data}
+            status="pending"
+            setOpenConfirmPopup={setOpenConfirmPopup}
+            setIdClickedSubscriber={setIdClickedSubscriber}
+            handlePopup={handlePopup}
+          />
 
-      <FilterStatusSubscribers
-        generalDataHeadTable={generalDataHeadTable}
-        subscribersData={subscribersData}
-        status="blocked"
-        setOpenConfirmPopup={setOpenConfirmPopup}
-        setIdClickedSubscriber={setIdClickedSubscriber}
-        handlePopup={handlePopup}
-      />
+          <FilterStatusSubscribers
+            generalDataHeadTable={generalDataHeadTable}
+            subscribersData={subscribersData.data}
+            status="blocked"
+            setOpenConfirmPopup={setOpenConfirmPopup}
+            setIdClickedSubscriber={setIdClickedSubscriber}
+            handlePopup={handlePopup}
+          />
+        </>
+      ) : (
+        <Error
+          titleOne="ERROR MESSAGE"
+          titleTwo="Probably you have got internet connection problem."
+          titleThree="Contact with your support team."
+        />
+      )}
     </>
   );
 };
