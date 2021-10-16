cc8a81f40cbc95e7dd407a4c1e1391b7acbb7e9c
Szymon Rojek <sz.rojek@gmail.com>
2021-10-16 11:53:21 +0200

    create EmailCampaignsList, add props

--- src/pages/pagesApp/EmailCampaignsList.js
+++ src/pages/pagesApp/EmailCampaignsList.js
@@ -1,5 +1,42 @@
-const EmailCampaignsList = () => {
-  return <p>Emails List</p>;
+import { TableCell } from "@material-ui/core";
+import { dataHeadEmailTable } from "../../data/dataHeadEmailTable";
+
+import {
+  ContainerTable,
+  HeadTable,
+  BodyTable,
+  RowCampaign,
+} from "../../components/Table";
+
+const EmailCampaignsList = ({
+  campaignsData,
+  setIdClickedItem,
+  setOpenConfirmPopup,
+  setContentPopup,
+}) => {
+  return (
+    <ContainerTable>
+      <HeadTable dataHeadTable={dataHeadEmailTable} />
+      <BodyTable>
+        {campaignsData.data &&
+          campaignsData.data.map((campaign, index) => (
+            <RowCampaign
+              children={
+                <TableCell component="th" scope="row">
+                  {++index}.
+                </TableCell>
+              }
+              key={`id-${campaign.id}`}
+              campaign={campaign}
+              index={index}
+              setOpenConfirmPopup={setOpenConfirmPopup}
+              setIdClickedItem={setIdClickedItem}
+              setContentPopup={setContentPopup}
+            />
+          ))}
+      </BodyTable>
+    </ContainerTable>
+  );
 };
 
 export default EmailCampaignsList;
