8c155e8a63ae7306fb99e3e592a91df2cbb64aa6
Szymon Rojek <sz.rojek@gmail.com>
2021-10-16 14:02:47 +0200

    rename ContainerTableSubscribers to ContainerSubscribers

---
+++ src/components/ContainerSubscribers/ContainerSubscribers.js
@@ -0,0 +1,37 @@
+import { TableCell } from "@material-ui/core";
+
+import { ContainerTable, HeadTable, BodyTable, RowSubscriber } from "../Table";
+
+const ContainerSubscribers = ({
+  dataHeadTable,
+  subscribersData,
+  setIdClickedItem,
+  handleSubscriberDetails,
+  setContentPopup,
+  setOpenConfirmPopup,
+}) => (
+  <ContainerTable>
+    <HeadTable dataHeadTable={dataHeadTable} />
+    <BodyTable>
+      {subscribersData &&
+        subscribersData.map((subscriber, index) => (
+          <RowSubscriber
+            children={
+              <TableCell component="th" scope="row">
+                {++index}.
+              </TableCell>
+            }
+            key={`id-${subscriber.id}`}
+            subscriber={subscriber}
+            index={index}
+            setIdClickedItem={setIdClickedItem}
+            handleSubscriberDetails={handleSubscriberDetails}
+            setContentPopup={setContentPopup}
+            setOpenConfirmPopup={setOpenConfirmPopup}
+          />
+        ))}
+    </BodyTable>
+  </ContainerTable>
+);
+
+export default ContainerSubscribers;
---
+++ src/components/ContainerSubscribers/index.js
@@ -0,0 +1,1 @@
+export { default as ContainerSubscribers } from "./ContainerSubscribers";
