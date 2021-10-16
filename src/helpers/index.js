e343dbb2fbf059e95a550d032dcd2bbf17bd749e
Szymon Rojek <sz.rojek@gmail.com>
2021-10-16 11:48:30 +0200

    remove handlers and function setContentPopup

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -2,7 +2,6 @@ export { default as getStatusColor } from "./getStatusColor";
 export { default as getFormattedData } from "./getFormattedData";
 export { default as isEven } from "./isEven";
 export { default as validationSchema } from "./validationSchema";
-export { default as setContentPopup } from "./setContentPopup";
 export { default as sortDataAlphabetically } from "./sortDataAlphabetically";
 export { default as capitalizeFirstLetter } from "./capitalizeFirstLetter";
 export { default as getLatestAddedSubscriber } from "./getLatestAddedSubscriber";
