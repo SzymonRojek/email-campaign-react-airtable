71af83a021328d5aec7a7f27a4714e9bb1b00f50
Szymon Rojek <sz.rojek@gmail.com>
2021-10-21 17:55:38 +0200

    add props multiline and rows

--- src/components/TextInput/TextInput.js
+++ src/components/TextInput/TextInput.js
@@ -1,25 +1,25 @@
 import { TextField, Typography } from "@mui/material";
 import { Grid } from "@material-ui/core";
 
-const TextInput = ({ error, register, value, message }) => {
-  return (
-    <Grid item xs={12}>
-      <TextField
-        required
-        id={value}
-        name={value}
-        label={value}
-        fullWidth
-        variant="filled"
-        margin="dense"
-        {...register(value)}
-        error={error}
-      />
-      <Typography variant="inherit" color="textSecondary">
-        {message}
-      </Typography>
-    </Grid>
-  );
-};
+const TextInput = ({ error, register, value, message, multiline, rows }) => (
+  <Grid item xs={12}>
+    <TextField
+      multiline={multiline}
+      rows={rows}
+      required
+      id={value}
+      name={value}
+      label={value}
+      fullWidth
+      variant="filled"
+      margin="dense"
+      {...register(value)}
+      error={error}
+    />
+    <Typography variant="inherit" color="textSecondary">
+      {message}
+    </Typography>
+  </Grid>
+);
 
 export default TextInput;
