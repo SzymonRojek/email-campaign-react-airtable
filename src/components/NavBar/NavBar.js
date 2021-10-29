77bda5c5f1028bd04c7716663645f386d5d88729
Szymon Rojek <sz.rojek@gmail.com>
2021-10-29 21:47:24 +0200

    add styles to navbar links when clicked

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -6,8 +6,42 @@ import { AiFillMail } from "react-icons/ai";
 import { AiFillHome } from "react-icons/ai";
 import { useNavigate } from "react-router";
 
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    // backgroundColor: theme.palette.background.paper,
+    // width: 500,
+  },
+  indicator: {
+    background: "none",
+  },
+  tabs: {
+    "& button[aria-selected='true']": {
+      position: "relative",
+
+      "&:before": {
+        content: '""',
+        position: "absolute",
+        left: 0,
+        top: 0,
+        right: 0,
+        bottom: 0,
+        background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
+        zIndex: 0,
+      },
+
+      "& > *": { zIndex: 0 },
+      // "& > .MuiTab-wrapper": {
+      //   height: "100%",
+      // },
+    },
+  },
+}));
+
 function NavBar() {
   const navigate = useNavigate();
+  const classes = useStyles();
   const [value, setValue] = useState(2);
 
   const handleClickTab = (e, newValue) => {
@@ -35,9 +69,12 @@ function NavBar() {
           <Typography>
             <GoMailRead />
           </Typography>
+
           <Tabs
             onChange={handleClickTab}
             indicatorColor="secondary"
+            className={classes.tabs}
+            classes={{ indicator: classes.indicator }}
             value={value}
           >
             <Tab
