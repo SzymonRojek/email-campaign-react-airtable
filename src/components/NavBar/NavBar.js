c7b76835b9bd733a66c492b83f864699392d2157
Szymon Rojek <sz.rojek@gmail.com>
2021-10-29 18:39:17 +0200

    add function navigateLinksOnReload - when page was reload and useEffect

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,12 +1,5 @@
-import { useState } from "react";
-import {
-  AppBar,
-  Typography,
-  Tabs,
-  Tab,
-  Toolbar,
-  Menu,
-} from "@material-ui/core";
+import { useEffect, useState } from "react";
+import { AppBar, Typography, Tabs, Tab, Toolbar } from "@material-ui/core";
 import { GoMailRead } from "react-icons/go";
 import { BsFillPersonPlusFill } from "react-icons/bs";
 import { AiFillMail } from "react-icons/ai";
@@ -15,9 +8,23 @@ import { useNavigate } from "react-router";
 
 function NavBar() {
   const navigate = useNavigate();
-  const [value, setValue] = useState(0);
+  const [value, setValue] = useState(2);
 
-  const handleClickTab = (e, newValue) => setValue(newValue);
+  const handleClickTab = (e, newValue) => {
+    return setValue(newValue);
+  };
+
+  const navigateLinksOnReload = (value) => {
+    value === 0
+      ? navigate("/subscribers")
+      : value === 1
+      ? navigate("/campaigns")
+      : navigate("/");
+  };
+
+  useEffect(() => {
+    navigateLinksOnReload(value);
+  }, []);
 
   const handleTabClick = (pageURL) => navigate(pageURL);
 
