4ce346f9e3b81f61d0936bf42bf333046804f49c
Szymon Rojek <sz.rojek@gmail.com>
2021-11-04 21:07:24 +0000

    add Container

--- src/pages/subscribers/NewSubscriber.js
+++ src/pages/subscribers/NewSubscriber.js
@@ -1,7 +1,8 @@
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
-import { Paper, Button, Box } from "@mui/material";
-import { Grid, Typography, Container } from "@material-ui/core";
+
+import { Paper, Box, Container } from "@mui/material";
+import { Grid, Typography } from "@material-ui/core";
 
 import api from "../../api";
 import { TextInput } from "../../components/TextInput";
@@ -50,54 +51,56 @@ const AddSubscriber = ({
   };
 
   return (
-    <Paper
-      elevation={6}
-      style={{
-        maxWidth: 600,
-        margin: "100px auto",
-        borderRadius: 8,
-      }}
-    >
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <Box px={3} py={3}>
-          <Grid container spacing={4}>
-            <Grid item xs={12}>
-              <Typography color="textSecondary" variant="body2">
-                *Fields required
-              </Typography>
-            </Grid>
-            <Grid item xs={12}>
-              <TextInput
-                value="name"
-                register={register}
-                error={!!errors?.name}
-                message={errors.name?.message ?? ""}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextInput
-                value="surname"
-                register={register}
-                error={!!errors?.surname}
-                message={errors.surname?.message ?? ""}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextInput
-                value="profession"
-                register={register}
-                error={!!errors?.profession}
-                message={errors.profession?.message ?? ""}
-              />
-            </Grid>
+    <Container>
+      <Paper
+        elevation={6}
+        style={{
+          maxWidth: 600,
+          margin: "100px auto",
+          borderRadius: 8,
+        }}
+      >
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Box px={3} py={3}>
+            <Grid container spacing={4}>
+              <Grid item xs={12}>
+                <Typography color="textSecondary" variant="body2">
+                  *Fields required
+                </Typography>
+              </Grid>
+              <Grid item xs={12}>
+                <TextInput
+                  value="name"
+                  register={register}
+                  error={!!errors?.name}
+                  message={errors.name?.message ?? ""}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextInput
+                  value="surname"
+                  register={register}
+                  error={!!errors?.surname}
+                  message={errors.surname?.message ?? ""}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextInput
+                  value="profession"
+                  register={register}
+                  error={!!errors?.profession}
+                  message={errors.profession?.message ?? ""}
+                />
+              </Grid>
 
-            <Grid item xs={12} md={12}>
-              <StyledButton label="add" ariaLabel="add" type="submit" />
+              <Grid item xs={12} md={12}>
+                <StyledButton label="add" ariaLabel="add" type="submit" />
+              </Grid>
             </Grid>
-          </Grid>
-        </Box>
-      </form>
-    </Paper>
+          </Box>
+        </form>
+      </Paper>
+    </Container>
   );
 };
 
