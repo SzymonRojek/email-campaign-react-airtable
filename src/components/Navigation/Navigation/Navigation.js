c29ca7827a37b85cc8c34ea9f219cdaf8debe557
Szymon Rojek <sz.rojek@gmail.com>
2021-11-04 16:44:45 +0000

    add new styles

--- src/components/Navigation/Navigation/Navigation.js
+++ src/components/Navigation/Navigation/Navigation.js
@@ -1,58 +1,66 @@
 import { Outlet } from "react-router-dom";
+import { Grid } from "@mui/material";
 
 import "./styles.css";
 import Menu from "../Menu/Menu";
 import NavLink from "../NavLink/NavLink";
-// import { DrawerMenu } from "../../DrawerMenu";
 
 const common = {
-  margin: 15,
   padding: "10px 15px",
   fontSize: 20,
   fontWeight: 600,
   letterSpacing: 1,
   textDecoration: "none",
-  borderRadius: 4,
 };
 
 const active = {
-  color: "#303f9f",
-  borderTop: "1px solid #303f9f",
-  borderBottom: "1px solid #303f9f",
+  color: "#003049",
+  borderRadius: 0,
+  borderBottom: "3px solid #003049",
   ...common,
 };
 
 const inactive = {
   color: "#fff",
-  backgroundColor: "#303f9f",
+  backgroundColor: "#003049",
+  borderRadius: 4,
   ...common,
 };
 
-const Navigation = ({ dataLinks, className }) => (
+const Navigation = ({ dataLinks }) => (
   <>
-    <div className={className}>
-      <Menu>
-        <ul className="header-list">
-          {dataLinks.map(({ to, exact, name }) => (
-            <li key={name}>
-              <NavLink
-                to={to}
-                exact={exact}
-                active={active}
-                inactive={inactive}
-              >
-                {name}
-              </NavLink>
-            </li>
-          ))}
-        </ul>
-      </Menu>
-    </div>
+    <Menu>
+      <Grid
+        style={{
+          margin: "90px auto",
+          maxWidth: 950,
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-around",
+        }}
+        container
+        className="header-list"
+        spacing={3}
+      >
+        {dataLinks.map(({ to, exact, name }) => (
+          <Grid item key={name} xs={12} md={3}>
+            <NavLink
+              to={to}
+              exact={exact}
+              active={active}
+              inactive={inactive}
+              className="link"
+            >
+              {name}
+            </NavLink>
+          </Grid>
+        ))}
+      </Grid>
+    </Menu>
+
     <div>
       <Outlet />
     </div>
-
-    {/* <DrawerMenu /> */}
   </>
 );
 
