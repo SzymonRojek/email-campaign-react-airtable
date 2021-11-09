177cfcaea176e3babfaa05d43dfde82b732906fd
Szymon Rojek <sz.rojek@gmail.com>
2021-11-09 20:55:23 +0000

    add Button ui and inline styles

--- src/components/Navigation/NavLink/NavLink.js
+++ src/components/Navigation/NavLink/NavLink.js
@@ -1,6 +1,7 @@
 import { Link, useLocation, useResolvedLocation } from "react-router-dom";
+import { Button } from "@mui/material";
 
-const NavLink = ({ to, exact, style, active, inactive, ...rest }) => {
+const NavLink = ({ to, exact, style, active, inactive, name, ...rest }) => {
   const location = useLocation();
   const resolvedLocation = useResolvedLocation(to);
 
@@ -11,7 +12,33 @@ const NavLink = ({ to, exact, style, active, inactive, ...rest }) => {
     isActive = location.pathname.startsWith(resolvedLocation.pathname);
   }
 
-  return <Link style={isActive ? active : inactive} to={to} {...rest} />;
+  return (
+    <Link style={isActive ? active : inactive} to={to} {...rest}>
+      <Button
+        disableRipple
+        disableElevation
+        style={
+          isActive
+            ? {
+                color: "#003049",
+                width: 100,
+                "&.MuiButtonBase-root:hover": {
+                  backgroundColor: "transparent",
+                },
+              }
+            : {
+                color: "#fff",
+                width: 100,
+                "&.MuiButtonBase-root:hover": {
+                  backgroundColor: "transparent",
+                },
+              }
+        }
+      >
+        {name}
+      </Button>
+    </Link>
+  );
 };
 
 export default NavLink;
