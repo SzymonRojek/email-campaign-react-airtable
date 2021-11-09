b23de74ddbab3eea72bfa732262d9d950616b88d
Szymon Rojek <sz.rojek@gmail.com>
2021-11-09 21:01:17 +0000

    remove styled, and ButtonStyled

--- src/components/StyledButton/StyledButton.js
+++ src/components/StyledButton/StyledButton.js
@@ -1,15 +1,5 @@
 import { Button } from "@mui/material";
-import { styled } from "@mui/styles";
 import { makeStyles } from "@material-ui/core/styles";
-
-const ButtonStyled = styled(Button)({
-  background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
-  border: 0,
-  borderRadius: 3,
-  boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
-  minHeight: 60,
-  width: "100%",
-});
 
 const useStyles = makeStyles(() => ({
   root: {
@@ -21,17 +11,6 @@ const useStyles = makeStyles(() => ({
     width: "100%",
   },
 }));
-
-// const StyledButton = ({ type, onClick, ariaLabel, label }) => (
-//   <ButtonStyled
-//     style={{ color: "white" }}
-//     type={type}
-//     aria-label={ariaLabel}
-//     onClick={onClick}
-//   >
-//     {label}
-//   </ButtonStyled>
-// );
 
 const StyledButton = ({ type, onClick, ariaLabel, label }) => {
   const classes = useStyles();
