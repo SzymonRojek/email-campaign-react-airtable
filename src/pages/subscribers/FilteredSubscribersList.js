7c4398fdda0da16df46791398fd97a796b1889fe
Szymon Rojek <sz.rojek@gmail.com>
2021-11-11 17:38:00 +0000

    add h1 with styles inline, add subHeading individual props to the FilterStatusSubscribers components

--- src/pages/subscribers/FilteredSubscribersList.js
+++ src/pages/subscribers/FilteredSubscribersList.js
@@ -15,62 +15,75 @@ const FilteredSubscribersList = (props) => {
   } = props;
 
   return (
-    <Container>
-      {subscribersData.status === "loading" ? (
-        <Loader title="Subscribers Data List is loading..." />
-      ) : subscribersData.status === "success" ? (
-        <>
-          {subscribersData.status === "success" &&
-          !subscribersData.data.length ? (
-            <Error
-              titleOne="There are not subscribers added yet."
-              titleTwo="Please add a new subscriber."
-            />
-          ) : (
-            <>
-              <FilterStatusSubscribers
-                heading="Active Status:"
-                generalDataHeadTable={generalDataHeadTable}
-                subscribersData={subscribersData.data}
-                status="active"
-                setSelectedData={setSelectedData}
-                handleSubscriberDetails={handleSubscriberDetails}
-                setContentPopup={setContentPopup}
-                setOpenConfirmPopup={setOpenConfirmPopup}
+    <>
+      <h1
+        style={{
+          textAlign: "center",
+          margin: "100px 0 60px 0",
+          color: "#003049",
+          letterSpacing: 2,
+          wordSpacing: 15,
+        }}
+      >
+        Subscribers status:
+      </h1>
+      <Container>
+        {subscribersData.status === "loading" ? (
+          <Loader title="Subscribers Data List is loading..." />
+        ) : subscribersData.status === "success" ? (
+          <>
+            {subscribersData.status === "success" &&
+            !subscribersData.data.length ? (
+              <Error
+                titleOne="There are not subscribers added yet."
+                titleTwo="Please add a new subscriber."
               />
+            ) : (
+              <>
+                <FilterStatusSubscribers
+                  subHeading="Active Status:"
+                  generalDataHeadTable={generalDataHeadTable}
+                  subscribersData={subscribersData.data}
+                  status="active"
+                  setSelectedData={setSelectedData}
+                  handleSubscriberDetails={handleSubscriberDetails}
+                  setContentPopup={setContentPopup}
+                  setOpenConfirmPopup={setOpenConfirmPopup}
+                />
 
-              <FilterStatusSubscribers
-                heading="Pending Status:"
-                generalDataHeadTable={generalDataHeadTable}
-                subscribersData={subscribersData.data}
-                status="pending"
-                setSelectedData={setSelectedData}
-                handleSubscriberDetails={handleSubscriberDetails}
-                setContentPopup={setContentPopup}
-                setOpenConfirmPopup={setOpenConfirmPopup}
-              />
+                <FilterStatusSubscribers
+                  subHeading="Pending Status:"
+                  generalDataHeadTable={generalDataHeadTable}
+                  subscribersData={subscribersData.data}
+                  status="pending"
+                  setSelectedData={setSelectedData}
+                  handleSubscriberDetails={handleSubscriberDetails}
+                  setContentPopup={setContentPopup}
+                  setOpenConfirmPopup={setOpenConfirmPopup}
+                />
 
-              <FilterStatusSubscribers
-                heading="Blocked Status:"
-                generalDataHeadTable={generalDataHeadTable}
-                subscribersData={subscribersData.data}
-                status="blocked"
-                setSelectedData={setSelectedData}
-                handleSubscriberDetails={handleSubscriberDetails}
-                setContentPopup={setContentPopup}
-                setOpenConfirmPopup={setOpenConfirmPopup}
-              />
-            </>
-          )}
-        </>
-      ) : (
-        <Error
-          titleOne="ERROR MESSAGE"
-          titleTwo="Probably there is no an access to the internet."
-          titleThree="Contact with your support team."
-        />
-      )}
-    </Container>
+                <FilterStatusSubscribers
+                  subHeading="Blocked Status:"
+                  generalDataHeadTable={generalDataHeadTable}
+                  subscribersData={subscribersData.data}
+                  status="blocked"
+                  setSelectedData={setSelectedData}
+                  handleSubscriberDetails={handleSubscriberDetails}
+                  setContentPopup={setContentPopup}
+                  setOpenConfirmPopup={setOpenConfirmPopup}
+                />
+              </>
+            )}
+          </>
+        ) : (
+          <Error
+            titleOne="ERROR MESSAGE"
+            titleTwo="Probably there is no an access to the internet."
+            titleThree="Contact with your support team."
+          />
+        )}
+      </Container>
+    </>
   );
 };
 
