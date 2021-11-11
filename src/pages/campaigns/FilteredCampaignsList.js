d77310c76a6ffc4f3b6447d6965e5e6362a3e54e
Szymon Rojek <sz.rojek@gmail.com>
2021-11-11 17:51:37 +0000

    add h1 with styles inline, change status props to subHeading

--- src/pages/campaigns/FilteredCampaignsList.js
+++ src/pages/campaigns/FilteredCampaignsList.js
@@ -15,50 +15,64 @@ const FilteredCampaignsList = (props) => {
   } = props;
 
   return (
-    <Container>
-      {campaignsData.status === "loading" ? (
-        <Loader title="Subscribers Data List is loading..." />
-      ) : campaignsData.status === "success" ? (
-        <>
-          {campaignsData.status === "success" && !campaignsData.data.length ? (
-            <Error
-              titleOne="There are not campaigns added yet."
-              titleTwo="Please add a new campaign."
-            />
-          ) : (
-            <>
-              <FilterStatusCampaigns
-                heading="Sent Status:"
-                dataHeadEmailTable={dataHeadEmailTable}
-                campaignsData={campaignsData.data}
-                status="sent"
-                setSelectedData={setSelectedData}
-                handleEditCampaign={handleEditCampaign}
-                setContentPopup={setContentPopup}
-                setOpenConfirmPopup={setOpenConfirmPopup}
+    <>
+      <h1
+        style={{
+          textAlign: "center",
+          margin: "100px 0 60px 0",
+          color: "#003049",
+          letterSpacing: 2,
+          wordSpacing: 15,
+        }}
+      >
+        Campaigns status:
+      </h1>
+      <Container>
+        {campaignsData.status === "loading" ? (
+          <Loader title="Subscribers Data List is loading..." />
+        ) : campaignsData.status === "success" ? (
+          <>
+            {campaignsData.status === "success" &&
+            !campaignsData.data.length ? (
+              <Error
+                titleOne="There are not campaigns added yet."
+                titleTwo="Please add a new campaign."
               />
+            ) : (
+              <>
+                <FilterStatusCampaigns
+                  subHeading="Sent:"
+                  dataHeadEmailTable={dataHeadEmailTable}
+                  campaignsData={campaignsData.data}
+                  status="sent"
+                  setSelectedData={setSelectedData}
+                  handleEditCampaign={handleEditCampaign}
+                  setContentPopup={setContentPopup}
+                  setOpenConfirmPopup={setOpenConfirmPopup}
+                />
 
-              <FilterStatusCampaigns
-                heading="Draft Status:"
-                dataHeadEmailTable={dataHeadEmailTable}
-                campaignsData={campaignsData.data}
-                status="draft"
-                setSelectedData={setSelectedData}
-                handleEditCampaign={handleEditCampaign}
-                setContentPopup={setContentPopup}
-                setOpenConfirmPopup={setOpenConfirmPopup}
-              />
-            </>
-          )}
-        </>
-      ) : (
-        <Error
-          titleOne="ERROR MESSAGE"
-          titleTwo="Probably there is no an access to the internet."
-          titleThree="Contact with your internet provider."
-        />
-      )}
-    </Container>
+                <FilterStatusCampaigns
+                  subHeading="Draft:"
+                  dataHeadEmailTable={dataHeadEmailTable}
+                  campaignsData={campaignsData.data}
+                  status="draft"
+                  setSelectedData={setSelectedData}
+                  handleEditCampaign={handleEditCampaign}
+                  setContentPopup={setContentPopup}
+                  setOpenConfirmPopup={setOpenConfirmPopup}
+                />
+              </>
+            )}
+          </>
+        ) : (
+          <Error
+            titleOne="ERROR MESSAGE"
+            titleTwo="Probably there is no an access to the internet."
+            titleThree="Contact with your internet provider."
+          />
+        )}
+      </Container>
+    </>
   );
 };
 
