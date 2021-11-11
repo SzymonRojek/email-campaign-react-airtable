6e459851b472b14ef50f04524e18b79bf3e3ab2c
Szymon Rojek <sz.rojek@gmail.com>
2021-11-11 18:12:01 +0000

    add backgroundColor as a prop

--- src/components/StyledButton/StyledButton.js
+++ src/components/StyledButton/StyledButton.js
@@ -1,22 +1,26 @@
 import { Button } from "@mui/material";
 import { makeStyles } from "@material-ui/core/styles";
 
-const useStyles = makeStyles(() => ({
-  root: {
-    background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
-    border: 0,
-    borderRadius: 3,
-    boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
-    minHeight: 60,
-    width: "100%",
-  },
-}));
+const StyledButton = ({ type, onClick, ariaLabel, label, backgroundColor }) => {
+  const useStyles = makeStyles(() => ({
+    root: {
+      border: 0,
+      borderRadius: 3,
+      boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
+      minHeight: 60,
+      width: "100%",
+    },
+  }));
+  //"linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)"
 
-const StyledButton = ({ type, onClick, ariaLabel, label }) => {
   const classes = useStyles();
   return (
     <Button
-      style={{ color: "white" }}
+      style={{
+        color: "white",
+        backgroundColor: backgroundColor,
+        fontSize: "1.5em",
+      }}
       type={type}
       aria-label={ariaLabel}
       onClick={onClick}
