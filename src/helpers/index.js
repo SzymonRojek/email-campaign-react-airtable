9e61481ee8be9a654d8967df9cdee3a0d5abb8f7
Szymon Rojek <sz.rojek@gmail.com>
2021-11-11 13:24:55 +0000

    rename validationCampaigns to validationCampaign

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -2,7 +2,7 @@ export { default as getStatusColor } from "./getStatusColor";
 export { default as getFormattedData } from "./getFormattedData";
 export { default as isEven } from "./isEven";
 export { default as validationSubscriber } from "./validationSubscriber";
-export { default as validationCampaign } from "./validationCampaigns";
+export { default as validationCampaign } from "./validationCampaign";
 export { default as sortDataAlphabetically } from "./sortDataAlphabetically";
 export { default as capitalizeFirstLetter } from "./capitalizeFirstLetter";
 export { default as getLatestAddedSubscriber } from "./getLatestAddedSubscriber";
