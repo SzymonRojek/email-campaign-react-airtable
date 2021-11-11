93e03c9570d62f0d3c15bf4e58c3674fb04c0f91
Szymon Rojek <sz.rojek@gmail.com>
2021-11-11 13:21:47 +0000

    rename export validationSchema to validationSubscriber

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,7 +1,7 @@
 export { default as getStatusColor } from "./getStatusColor";
 export { default as getFormattedData } from "./getFormattedData";
 export { default as isEven } from "./isEven";
-export { default as validationSchema } from "./validationSchema";
+export { default as validationSubscriber } from "./validationSubscriber";
 export { default as validationCampaign } from "./validationCampaigns";
 export { default as sortDataAlphabetically } from "./sortDataAlphabetically";
 export { default as capitalizeFirstLetter } from "./capitalizeFirstLetter";
