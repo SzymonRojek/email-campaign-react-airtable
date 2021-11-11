c1977aece5c3ec278a1eda33b051b5b918969630
Szymon Rojek <sz.rojek@gmail.com>
2021-11-11 13:15:26 +0000

    remove exports

--- src/pages/campaigns/index.js
+++ src/pages/campaigns/index.js
@@ -2,4 +2,3 @@ export { default as CampaignsList } from "./CampaignsList";
 export { default as FilteredCampaignsList } from "./FilteredCampaignsList";
 export { default as EditCampaign } from "./EditCampaign";
 export { default as NewCampaign } from "./NewCampaign";
-export { default as Campaigns } from "./Campaigns";
--- src/pages/subscribers/index.js
+++ src/pages/subscribers/index.js
@@ -2,4 +2,3 @@ export { default as SubscribersList } from "./SubscribersList";
 export { default as FilteredSubscribersList } from "./FilteredSubscribersList";
 export { default as SubscriberDetails } from "./SubscriberDetails";
 export { default as NewSubscriber } from "./NewSubscriber";
-export { default as Subscribers } from "./Subscribers";
