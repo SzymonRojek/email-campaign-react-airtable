493c26617da269e759cee24cab0b9551bd4adf1e
Szymon Rojek <sz.rojek@gmail.com>
2021-11-12 12:54:34 +0000

    create new StyledTextField with all styles

--- src/components/TextInput/TextInput.js
+++ src/components/TextInput/TextInput.js
@@ -1,9 +1,35 @@
 import { TextField, Typography } from "@mui/material";
 import { Grid } from "@material-ui/core";
+import { inputLabelClasses } from "@mui/material/InputLabel";
+import { withStyles } from "@mui/styles";
+import "./styles.css";
+
+const StyledTextField = withStyles({
+  root: {
+    "& .MuiInputBase-root": {
+      color: "white",
+    },
+    "& label.Mui-focused": {
+      color: "#fff",
+    },
+    "& .MuiOutlinedInput-root": {
+      "& fieldset": {
+        borderColor: "#fff",
+      },
+      "&:hover fieldset": {
+        borderColor: "#fff",
+      },
+      "&.Mui-focused fieldset": {
+        borderColor: "green",
+      },
+    },
+  },
+})(TextField);
 
 const TextInput = ({ error, register, value, message, multiline, rows }) => (
   <Grid item xs={12}>
-    <TextField
+    <StyledTextField
+      variant="outlined"
       multiline={multiline}
       rows={rows}
       required
@@ -11,12 +37,21 @@ const TextInput = ({ error, register, value, message, multiline, rows }) => (
       name={value}
       label={value}
       fullWidth
-      variant="filled"
       margin="dense"
       {...register(value)}
       error={error}
+      InputLabelProps={{
+        sx: {
+          // set the color of the label when not shrinked
+          color: "#fff",
+          [`&.${inputLabelClasses.shrink}`]: {
+            // set the color of the label when shrinked (usually when the TextField is focused)
+            color: "#fff",
+          },
+        },
+      }}
     />
-    <Typography variant="inherit" color="textSecondary">
+    <Typography variant="inherit" style={{ color: "crimson", paddingTop: 4 }}>
       {message}
     </Typography>
   </Grid>
