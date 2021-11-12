c3a3770ffc2334284f23eebb8fc5a6740f3ce9fc
Szymon Rojek <sz.rojek@gmail.com>
2021-11-12 18:56:37 +0000

    change backgroundColor

--- src/components/Table/HeadTable.js
+++ src/components/Table/HeadTable.js
@@ -4,7 +4,7 @@ import { TableCell, TableHead, TableRow } from "@material-ui/core";
 const useStyles = makeStyles((theme) => ({
   tableHeaderCell: {
     fontWeight: "bold",
-    backgroundColor: theme.palette.primary.dark,
+    backgroundColor: "#142f43",
     color: theme.palette.getContrastText(theme.palette.primary.dark),
   },
 }));
