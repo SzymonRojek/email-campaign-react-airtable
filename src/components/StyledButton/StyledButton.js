2340aad322af2bc7ec2191edf62f39164280161c
Szymon Rojek <sz.rojek@gmail.com>
2021-11-12 18:11:04 +0000

    add new styles to the button

--- src/components/StyledButton/StyledButton.js
+++ src/components/StyledButton/StyledButton.js
@@ -1,34 +1,37 @@
 import { Button } from "@mui/material";
-import { makeStyles } from "@material-ui/core/styles";
+import { styled } from "@mui/material/styles";
 
-const StyledButton = ({ type, onClick, ariaLabel, label, backgroundColor }) => {
-  const useStyles = makeStyles(() => ({
-    root: {
-      border: 0,
-      borderRadius: 3,
-      boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
-      minHeight: 60,
-      width: "100%",
-    },
-  }));
+const CssButton = styled(Button)(({ backgroundColor, onHover }) => ({
+  ":hover": {
+    backgroundColor: onHover,
+  },
+  color: "white",
+  backgroundColor: backgroundColor,
+  minWidth: 120,
+  fontSize: 18,
+  padding: "15px 25px",
+  letterSpacing: 2,
+  margin: "20px 0",
+}));
 
-  const classes = useStyles();
+const StyledButton = ({
+  type,
+  onClick,
+  ariaLabel,
+  label,
+  backgroundColor,
+  onHover,
+}) => {
   return (
-    <Button
-      style={{
-        color: "white",
-        backgroundColor: backgroundColor,
-        fontSize: "1.5em",
-        letterSpacing: 2,
-      }}
+    <CssButton
+      onHover={onHover}
+      backgroundColor={backgroundColor}
       type={type}
       aria-label={ariaLabel}
       onClick={onClick}
-      className={classes.root}
     >
       {label}
-    </Button>
+    </CssButton>
   );
 };
-
 export default StyledButton;
