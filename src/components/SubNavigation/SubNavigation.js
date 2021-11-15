86f173e349f46fcba6ba3e2d343a55b7c3c5922f
Szymon Rojek <sz.rojek@gmail.com>
2021-11-15 18:12:49 +0000

    remove subNavigation on smallest devices

--- src/components/SubNavigation/SubNavigation.js
+++ src/components/SubNavigation/SubNavigation.js
@@ -1,35 +1,86 @@
 import { Outlet } from "react-router-dom";
 import { Container, Grid } from "@mui/material";
+import { useTheme } from "@material-ui/core/styles";
+import { useMediaQuery } from "@material-ui/core";
 
 import StyledLink from "./StyledLink";
 
-const SubNavigation = ({ dataLinks }) => (
-  <Container>
-    <nav>
-      <Grid
-        container
-        rowSpacing={{ xs: 1 }}
-        style={{
-          margin: "20px auto",
-          maxWidth: 400,
-        }}
-        direction="row"
-        justifyContent="space-between"
-        alignItems="center"
-      >
-        {dataLinks.map(({ to, exact, name }, index) => (
-          <Grid item key={index} xs={12} md={3}>
-            <StyledLink to={to} exact={exact}>
-              {name}
-            </StyledLink>
-          </Grid>
-        ))}
-      </Grid>
-    </nav>
+const SubNavigation = ({ dataLinks }) => {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("xs"));
 
-    <div>
-      <Outlet />
-    </div>
-  </Container>
-);
+  return (
+    <>
+      {!isMobile ? (
+        <Container>
+          <nav>
+            <Grid
+              container
+              style={{
+                margin: "20px auto",
+                maxWidth: 400,
+              }}
+              direction="row"
+              justifyContent="space-between"
+              alignItems="center"
+            >
+              {dataLinks.map(({ to, exact, name }, index) => (
+                <Grid item key={index} md={3}>
+                  <StyledLink to={to} exact={exact}>
+                    {name}
+                  </StyledLink>
+                </Grid>
+              ))}
+            </Grid>
+          </nav>
+
+          <div>
+            <Outlet />
+          </div>
+        </Container>
+      ) : (
+        <div>
+          <Outlet />
+        </div>
+      )}
+    </>
+  );
+};
+
 export default SubNavigation;
+
+// previous version
+// import { Outlet } from "react-router-dom";
+// import { Container, Grid } from "@mui/material";
+// import StyledLink from "./StyledLink";
+
+// const SubNavigation = ({ dataLinks }) => (
+//   <Container>
+//     <nav>
+//       <Grid
+//         container
+//         rowSpacing={{ xs: 1 }}
+//         style={{
+//           margin: "20px auto",
+//           maxWidth: 400,
+//         }}
+//         direction="row"
+//         justifyContent="space-between"
+//         alignItems="center"
+//       >
+//         {dataLinks.map(({ to, exact, name }, index) => (
+//           <Grid item key={index} xs={12} md={3}>
+//             <StyledLink to={to} exact={exact}>
+//               {name}
+//             </StyledLink>
+//           </Grid>
+//         ))}
+//       </Grid>
+//     </nav>
+
+//     <div>
+//       <Outlet />
+//     </div>
+//   </Container>
+// );
+// export default SubNavigation;
