81b116974d60c9ad7e71d12927445cb472f56e9a
Szymon Rojek <sz.rojek@gmail.com>
2021-11-15 23:25:11 +0000

    transfer styles into separate objects variables, change color od the text and icon

--- src/components/StyledFooter/StyledFooter.js
+++ src/components/StyledFooter/StyledFooter.js
@@ -3,18 +3,23 @@ import { AppBar, Typography } from "@material-ui/core";
 import { Link } from "@mui/material";
 import { BsGithub } from "react-icons/bs";
 
+const appBarStyles = {
+  position: "absolute",
+  bottom: 0,
+  left: 0,
+  backgroundColor: "#142f43",
+  color: "#ffffff8c",
+};
+
+const iconStyles = {
+  fontSize: 25,
+  cursor: "pointer",
+  color: "orange",
+};
+
 const StyledFooter = () => (
   <footer>
-    <AppBar
-      position="static"
-      color="primary"
-      style={{
-        position: "absolute",
-        bottom: 0,
-        left: 0,
-        backgroundColor: "#142f43",
-      }}
-    >
+    <AppBar position="static" color="primary" style={appBarStyles}>
       <Container maxWidth="md">
         <Box p={4}>
           <Grid
@@ -31,13 +36,7 @@ const StyledFooter = () => (
             </Grid>
             <Grid item xs={12}>
               <Link href="https://github.com/SzymonRojek" target="_blank">
-                <BsGithub
-                  style={{
-                    fontSize: 25,
-                    cursor: "pointer",
-                    color: "#fff",
-                  }}
-                />
+                <BsGithub style={iconStyles} />
               </Link>
             </Grid>
           </Grid>
