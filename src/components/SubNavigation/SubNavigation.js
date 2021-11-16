6cdc10e148e3b4f6f977e528c912450c511fea90
Szymon Rojek <sz.rojek@gmail.com>
2021-11-16 18:44:37 +0000

    remove comments

--- src/components/SubNavigation/SubNavigation.js
+++ src/components/SubNavigation/SubNavigation.js
@@ -46,41 +46,4 @@ const SubNavigation = ({ dataLinks }) => {
     </>
   );
 };
-
 export default SubNavigation;
-
-// previous version
-// import { Outlet } from "react-router-dom";
-// import { Container, Grid } from "@mui/material";
-// import StyledLink from "./StyledLink";
-
-// const SubNavigation = ({ dataLinks }) => (
-//   <Container>
-//     <nav>
-//       <Grid
-//         container
-//         rowSpacing={{ xs: 1 }}
-//         style={{
-//           margin: "20px auto",
-//           maxWidth: 400,
-//         }}
-//         direction="row"
-//         justifyContent="space-between"
-//         alignItems="center"
-//       >
-//         {dataLinks.map(({ to, exact, name }, index) => (
-//           <Grid item key={index} xs={12} md={3}>
-//             <StyledLink to={to} exact={exact}>
-//               {name}
-//             </StyledLink>
-//           </Grid>
-//         ))}
-//       </Grid>
-//     </nav>
-
-//     <div>
-//       <Outlet />
-//     </div>
-//   </Container>
-// );
-// export default SubNavigation;
