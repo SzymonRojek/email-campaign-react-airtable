7d8fc64fab600e541052a8140ecc2e8e759ab1d6
Szymon Rojek <sz.rojek@gmail.com>
2021-11-16 15:12:56 +0000

    add position absolute depends of the size breakpoints

--- src/components/StyledFooter/StyledFooter.js
+++ src/components/StyledFooter/StyledFooter.js
@@ -2,15 +2,8 @@ import { Container, Box, Grid } from "@material-ui/core";
 import { AppBar, Typography } from "@material-ui/core";
 import { Link } from "@mui/material";
 import { BsGithub } from "react-icons/bs";
-
-const appBarStyles = {
-  position: "absolute",
-  bottom: 0,
-  left: 0,
-  backgroundColor: "#142f43",
-  color: "#ffffff8c",
-  zIndex: -1,
-};
+import { useTheme } from "@material-ui/core/styles";
+import { useMediaQuery } from "@material-ui/core";
 
 const iconStyles = {
   fontSize: 25,
@@ -18,33 +11,45 @@ const iconStyles = {
   color: "orange",
 };
 
-const StyledFooter = () => (
-  <footer>
-    <AppBar position="static" color="primary" style={appBarStyles}>
-      <Container maxWidth="md">
-        <Box p={4}>
-          <Grid
-            container
-            direction="column"
-            justifyContent="center"
-            alignItems="center"
-            spacing={2}
-          >
-            <Grid item xs={12}>
-              <Typography variant="body1">
-                Coded By Szymon Rojek © 2021
-              </Typography>
+const StyledFooter = () => {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("xs"));
+
+  const appBarStyles = {
+    position: isMobile ? "" : "absolute",
+    bottom: 0,
+    backgroundColor: "#142f43",
+    color: "#ffffff8c",
+    zIndex: -1,
+  };
+  return (
+    <footer>
+      <AppBar position="static" color="primary" style={appBarStyles}>
+        <Container maxWidth="md">
+          <Box p={3}>
+            <Grid
+              container
+              direction="column"
+              justifyContent="center"
+              alignItems="center"
+              spacing={2}
+            >
+              <Grid item xs={12}>
+                <Typography variant="body1">
+                  Coded By Szymon Rojek © 2021
+                </Typography>
+              </Grid>
+              <Grid item xs={12}>
+                <Link href="https://github.com/SzymonRojek" target="_blank">
+                  <BsGithub style={iconStyles} />
+                </Link>
+              </Grid>
             </Grid>
-            <Grid item xs={12}>
-              <Link href="https://github.com/SzymonRojek" target="_blank">
-                <BsGithub style={iconStyles} />
-              </Link>
-            </Grid>
-          </Grid>
-        </Box>
-      </Container>
-    </AppBar>
-  </footer>
-);
+          </Box>
+        </Container>
+      </AppBar>
+    </footer>
+  );
+};
 
 export default StyledFooter;
