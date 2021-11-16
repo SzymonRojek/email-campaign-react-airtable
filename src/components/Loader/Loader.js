c5b3efa8505404d4fff990d5d6b9a0c106235072
Szymon Rojek <sz.rojek@gmail.com>
2021-11-16 18:42:56 +0000

    create main container

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -4,14 +4,14 @@ const Loader = ({ title }) => (
   <>
     <div className="loader-container">
       <div className="load"></div>
-    </div>
 
-    <div className="text-container">
-      <p className="loader-text">{title}</p>
+      <div className="text-container">
+        <p className="loader-text">{title}</p>
 
-      <div className="dot-container">
-        <p className="loader-text">Data</p>
-        <div className="dot-flashing"></div>
+        <div className="dot-container">
+          <p className="loader-text">Data</p>
+          <div className="dot-flashing"></div>
+        </div>
       </div>
     </div>
   </>
