30f7dde93731a774bcfa2fc85732d06fd5beab34
Szymon Rojek <sz.rojek@gmail.com>
2021-11-16 16:37:10 +0000

    remove position absolute, and useTheme with useMediaQuery hooks

--- src/components/StyledFooter/StyledFooter.js
+++ src/components/StyledFooter/StyledFooter.js
@@ -2,8 +2,13 @@ import { Container, Box, Grid } from "@material-ui/core";
 import { AppBar, Typography } from "@material-ui/core";
 import { Link } from "@mui/material";
 import { BsGithub } from "react-icons/bs";
-import { useTheme } from "@material-ui/core/styles";
-import { useMediaQuery } from "@material-ui/core";
+
+const appBarStyles = {
+  marginTop: "auto",
+  backgroundColor: "#142f43",
+  color: "#ffffff8c",
+  zIndex: -1,
+};
 
 const iconStyles = {
   fontSize: 25,
@@ -11,45 +16,33 @@ const iconStyles = {
   color: "orange",
 };
 
-const StyledFooter = () => {
-  const theme = useTheme();
-  const isMobile = useMediaQuery(theme.breakpoints.down("xs"));
-
-  const appBarStyles = {
-    position: isMobile ? "" : "absolute",
-    bottom: 0,
-    backgroundColor: "#142f43",
-    color: "#ffffff8c",
-    zIndex: -1,
-  };
-  return (
-    <footer>
-      <AppBar position="static" color="primary" style={appBarStyles}>
-        <Container maxWidth="md">
-          <Box p={3}>
-            <Grid
-              container
-              direction="column"
-              justifyContent="center"
-              alignItems="center"
-              spacing={2}
-            >
-              <Grid item xs={12}>
-                <Typography variant="body1">
-                  Coded By Szymon Rojek © 2021
-                </Typography>
-              </Grid>
-              <Grid item xs={12}>
-                <Link href="https://github.com/SzymonRojek" target="_blank">
-                  <BsGithub style={iconStyles} />
-                </Link>
-              </Grid>
+const StyledFooter = () => (
+  <footer>
+    <AppBar position="static" color="primary" style={appBarStyles}>
+      <Container maxWidth="md">
+        <Box p={3}>
+          <Grid
+            container
+            direction="column"
+            justifyContent="center"
+            alignItems="center"
+            spacing={2}
+          >
+            <Grid item xs={12}>
+              <Typography variant="body1">
+                Coded By Szymon Rojek © 2021
+              </Typography>
             </Grid>
-          </Box>
-        </Container>
-      </AppBar>
-    </footer>
-  );
-};
+            <Grid item xs={12}>
+              <Link href="https://github.com/SzymonRojek" target="_blank">
+                <BsGithub style={iconStyles} />
+              </Link>
+            </Grid>
+          </Grid>
+        </Box>
+      </Container>
+    </AppBar>
+  </footer>
+);
 
 export default StyledFooter;
