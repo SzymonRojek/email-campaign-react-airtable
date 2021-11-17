435e01e3939d161690e0c621df5baf9957a4a74b
Szymon Rojek <sz.rojek@gmail.com>
2021-11-17 14:37:30 +0000

    remove fragment

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,20 +1,18 @@
 import "./styles.css";
 
 const Loader = ({ title }) => (
-  <>
-    <div className="loader-container">
-      <div className="load"></div>
+  <div className="loader-container">
+    <div className="load"></div>
 
-      <div className="text-container">
-        <p className="loader-text">{title} Data</p>
+    <div className="text-container">
+      <p className="loader-text">{title} Data</p>
 
-        <div className="dot-container">
-          <p className="loader-text">loading</p>
-          <div className="dot-flashing"></div>
-        </div>
+      <div className="dot-container">
+        <p className="loader-text">loading</p>
+        <div className="dot-flashing"></div>
       </div>
     </div>
-  </>
+  </div>
 );
 
 export default Loader;
