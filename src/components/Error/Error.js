de8d1e1cd11ce3b994e82cf0c5125275ee7d7492
Szymon Rojek <sz.rojek@gmail.com>
2021-11-17 18:29:23 +0000

    remove div

--- src/components/Error/Error.js
+++ src/components/Error/Error.js
@@ -1,8 +1,7 @@
 import "./styles.css";
 
 const Error = ({ titleOne, titleTwo, titleThree }) => (
-  <div className="error-container" style={{ textAlign: "center" }}>
-    <div></div>
+  <div className="error-container">
     <p className="error-text error-mainText">{titleOne}</p>
     <p className="error-text error-subText">{titleTwo}</p>
     <p className="error-text error-subText">{titleThree}</p>
