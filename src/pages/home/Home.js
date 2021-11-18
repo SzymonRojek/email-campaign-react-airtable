8d8b6544ca628dfbbda546ad9b043aca26b6162d
Szymon Rojek <sz.rojek@gmail.com>
2021-11-18 13:32:37 +0000

    add useStyles and media querry

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,5 +1,7 @@
 import { Container, Box, Grid, Typography } from "@mui/material";
 
+import { makeStyles } from "@material-ui/core/styles";
+
 import homeImg from "../../img/image-home.svg";
 
 const boxStyle = {
@@ -10,39 +12,59 @@ const boxStyle = {
   borderRadius: 6,
 };
 
+const useStyles = makeStyles((theme) => ({
+  "@media (min-width:960px)": {
+    container: {
+      marginTop: 40,
+    },
+  },
+  "@media (max-width:600px)": {
+    container: {
+      marginTop: 40,
+    },
+    homeImg: {
+      maxWidth: 200,
+    },
+  },
+  mainHeading: {
+    color: "orange",
+  },
+}));
+
 const Home = () => {
+  const classes = useStyles();
+
   return (
-    <Container>
+    <Container className={classes.container}>
       <Box boxShadow={3} p={4} style={boxStyle}>
         <Grid container direction="row" spacing={4}>
-          <Grid item sm={12} xs={6} md={6}>
+          <Grid item sm={12} xs={12} md={6}>
             <Typography
               variant="h1"
-              color="textPrimary"
               gutterBottom
-              style={{ color: "orange" }}
               md={12}
+              className={classes.mainHeading}
             >
               Hello, there.
             </Typography>
           </Grid>
           <Grid item sm={6} md={6}>
-            <Typography variant="h4" paragraph>
+            <Typography variant="h2" paragraph>
               This is a personalize <strong>Email Campaign </strong>
               application. You can write @mail to all subscribers just by one
               CLICK - easy peasy!
             </Typography>
           </Grid>
           <Grid item sm={6} xs={12} md={6} order={{ md: 4 }}>
-            <Grid container>
+            <Grid container direction="row">
               <Grid item sm={12} xs={12}>
-                <img src={homeImg} alt="campaign" style={{ height: "auto" }} />
+                <img src={homeImg} alt="campaign" className={classes.homeImg} />
               </Grid>
             </Grid>
           </Grid>
           <Grid item xs={12} md={6} order={{ md: 3 }}>
             <Grid container>
-              <Typography variant="h5" paragraph>
+              <Typography variant="h3" paragraph>
                 Add new subscriber, wait for an admin confirmation to get an
                 active status, get more their details. Finally create an email
                 campaign and send it to all your subscribers by one simple
@@ -50,10 +72,9 @@ const Home = () => {
               </Typography>
             </Grid>
           </Grid>
-
           <Grid item xs={12} order={{ md: 5 }}>
             <Grid container>
-              <Typography variant="h6" paragraph>
+              <Typography variant="body2">
                 If you want to get more information please read this description
                 below:
               </Typography>
