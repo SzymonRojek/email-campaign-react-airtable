cc5a059c86abd45765deaf75490e449338bcd4ac
Szymon Rojek <sz.rojek@gmail.com>
2021-11-18 16:56:01 +0000

    add more text, styles, grid containers and items, positioned them on different creeen sizes

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,10 +1,11 @@
-import { Container, Box, Grid, Typography } from "@mui/material";
-
 import { makeStyles } from "@material-ui/core/styles";
+import { Link } from "@mui/material";
+import { Container, Box, Grid, Typography } from "@mui/material";
+import { SiAirtable } from "react-icons/si";
 
 import homeImg from "../../img/image-home.svg";
 
-const boxStyle = {
+const styledBox = {
   maxWidth: 1224,
   margin: "auto",
   color: "#ffffff8c",
@@ -12,10 +13,18 @@ const boxStyle = {
   borderRadius: 6,
 };
 
+const styledLink = {
+  color: "orange",
+  padding: "0 10px",
+};
+
 const useStyles = makeStyles((theme) => ({
   "@media (min-width:960px)": {
     container: {
       marginTop: 40,
+    },
+    mainHeading: {
+      padding: "100px",
     },
   },
   "@media (max-width:600px)": {
@@ -36,7 +45,7 @@ const Home = () => {
 
   return (
     <Container className={classes.container}>
-      <Box boxShadow={3} p={4} style={boxStyle}>
+      <Box boxShadow={3} p={4} style={styledBox}>
         <Grid container direction="row" spacing={4}>
           <Grid item sm={12} xs={12} md={6}>
             <Typography
@@ -56,28 +65,74 @@ const Home = () => {
             </Typography>
           </Grid>
           <Grid item sm={6} xs={12} md={6} order={{ md: 4 }}>
-            <Grid container direction="row">
-              <Grid item sm={12} xs={12}>
-                <img src={homeImg} alt="campaign" className={classes.homeImg} />
-              </Grid>
+            <Grid
+              container
+              direction="column"
+              alignItems="center"
+              justify="center"
+            >
+              <img src={homeImg} alt="campaign" className={classes.homeImg} />
             </Grid>
           </Grid>
           <Grid item xs={12} md={6} order={{ md: 3 }}>
-            <Grid container>
-              <Typography variant="h3" paragraph>
-                Add new subscriber, wait for an admin confirmation to get an
-                active status, get more their details. Finally create an email
-                campaign and send it to all your subscribers by one simple
-                click!
-              </Typography>
+            <Grid container spacing={3}>
+              <Grid item>
+                <Typography variant="h3">
+                  The app is connected to the
+                  <Link
+                    href="https://airtable.com/"
+                    target="_blank"
+                    underline="hover"
+                    style={styledLink}
+                  >
+                    <SiAirtable />
+                    Airtable
+                  </Link>
+                  base. Easily brings all information together, organize and
+                  connect it we want, and change it as needed.
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Typography variant="h3">
+                  Airtable base will provide its own <strong>rest API</strong>{" "}
+                  to create, read, update, and delete any records.
+                </Typography>
+              </Grid>
             </Grid>
           </Grid>
           <Grid item xs={12} order={{ md: 5 }}>
-            <Grid container>
-              <Typography variant="body2">
-                If you want to get more information please read this description
-                below:
-              </Typography>
+            <Grid container spacing={3}>
+              <Grid item>
+                <Typography variant="body2">
+                  Add new subscriber, wait for an admin confirmation to get an
+                  active status, get more their details. Finally create an email
+                  campaign and send it to all your subscribers by one simple
+                  click!
+                </Typography>
+              </Grid>
+
+              <Grid item>
+                <Typography variant="body2">
+                  If you would like to read the detailed description of the
+                  application or just to get more information about used
+                  technologies, please check out the
+                  <Link
+                    href="https://github.com/SzymonRojek/email-campaign-react-airtable"
+                    target="_blank"
+                    underline="hover"
+                    style={styledLink}
+                  >
+                    ReadMe
+                  </Link>
+                  file of this project.
+                </Typography>
+              </Grid>
+
+              <Grid item>
+                <Typography variant="body2" style={{ color: "orange" }}>
+                  Thanks! 🙏
+                </Typography>
+              </Grid>
             </Grid>
           </Grid>
         </Grid>
