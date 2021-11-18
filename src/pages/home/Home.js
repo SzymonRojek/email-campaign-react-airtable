9d951db6521616efea906f527d9c467e7b5a2323
Szymon Rojek <sz.rojek@gmail.com>
2021-11-18 11:23:41 +0000

    add container, order items in the specific way

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,4 +1,4 @@
-import { Box, Grid, Typography } from "@mui/material";
+import { Container, Box, Grid, Typography } from "@mui/material";
 
 import homeImg from "../../img/image-home.svg";
 
@@ -12,59 +12,56 @@ const boxStyle = {
 
 const Home = () => {
   return (
-    <Box boxShadow={3} m={4} p={4} style={boxStyle}>
-      <Grid container direction="row" spacing={4}>
-        <Grid item xs={12} sm={6}>
-          <Grid container>
-            <Grid item sm={12} xs={12}>
-              <Typography
-                variant="h1"
-                color="textPrimary"
-                gutterBottom
-                style={{ color: "orange" }}
-              >
-                Hello, there.
-              </Typography>
-              <Typography variant="h4" paragraph>
-                This is a personalize <strong>Email Campaign </strong>
-                application. You can write @mail to all subscribers just by one
-                CLICK - easy peasy!
-              </Typography>
-              <Typography variant="h5" paragraph>
-                Add new subscriber, wait for an admin confirmation to get an
-                active status, get more their details. Finally create an email
-                campaign and send it to all your subscribers by one simple
-                click!
-              </Typography>
-            </Grid>
-            {/* <Grid item xs={12}>
-              <Typography variant="h5" paragraph>
-                Add new subscriber, wait for an admin confirmation to get an
-                active status, get more their details. Finally create an email
-                campaign and send it to all your subscribers by one simple
-                click!
-              </Typography>
-            </Grid> */}
-          </Grid>
-        </Grid>
-
-        <Grid item sm={6} xs={12}>
-          <Grid container>
-            <Grid item sm={12} xs={12}>
-              <img src={homeImg} alt="campaign" style={{ height: "auto" }} />
-            </Grid>
-          </Grid>
-        </Grid>
-        <Grid item xs={12}>
-          <Grid container>
-            <Typography variant="h6" paragraph>
-              If you want to get more information please read this description
-              below:
+    <Container>
+      <Box boxShadow={3} p={4} style={boxStyle}>
+        <Grid container direction="row" spacing={4}>
+          <Grid item sm={12} xs={6} md={6}>
+            <Typography
+              variant="h1"
+              color="textPrimary"
+              gutterBottom
+              style={{ color: "orange" }}
+              md={12}
+            >
+              Hello, there.
             </Typography>
           </Grid>
+          <Grid item sm={6} md={6}>
+            <Typography variant="h4" paragraph>
+              This is a personalize <strong>Email Campaign </strong>
+              application. You can write @mail to all subscribers just by one
+              CLICK - easy peasy!
+            </Typography>
+          </Grid>
+          <Grid item sm={6} xs={12} md={6} order={{ md: 4 }}>
+            <Grid container>
+              <Grid item sm={12} xs={12}>
+                <img src={homeImg} alt="campaign" style={{ height: "auto" }} />
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item xs={12} md={6} order={{ md: 3 }}>
+            <Grid container>
+              <Typography variant="h5" paragraph>
+                Add new subscriber, wait for an admin confirmation to get an
+                active status, get more their details. Finally create an email
+                campaign and send it to all your subscribers by one simple
+                click!
+              </Typography>
+            </Grid>
+          </Grid>
+
+          <Grid item xs={12} order={{ md: 5 }}>
+            <Grid container>
+              <Typography variant="h6" paragraph>
+                If you want to get more information please read this description
+                below:
+              </Typography>
+            </Grid>
+          </Grid>
         </Grid>
-      </Grid>
-    </Box>
+      </Box>
+    </Container>
   );
 };
 
