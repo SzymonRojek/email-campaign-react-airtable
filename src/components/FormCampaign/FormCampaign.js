06982f26b94dd643abe7c612daa3b814bc3f2f65
Szymon Rojek <sz.rojek@gmail.com>
2021-11-19 19:05:02 +0000

    add styles into one  object, remove container (is added in the NewCampaign page),

--- src/components/FormCampaign/FormCampaign.js
+++ src/components/FormCampaign/FormCampaign.js
@@ -1,8 +1,17 @@
-import { Paper, Box, Container } from "@mui/material";
-import { Grid, Typography } from "@material-ui/core";
+import { Paper, Box, Grid, Typography } from "@material-ui/core";
 
 import { TextInput } from "../../components/TextInput";
 import { StyledButton } from "../StyledButton";
+
+const style = {
+  paper: {
+    maxWidth: 600,
+    margin: "20px auto",
+    borderRadius: 8,
+    backgroundColor: "#142F43",
+  },
+  typography: { color: "#fff" },
+};
 
 const FormCampaign = ({
   handleSubmit,
@@ -13,84 +22,74 @@ const FormCampaign = ({
   getCampaignsData,
 }) => {
   return (
-    <Container>
-      <Paper
-        elevation={14}
-        style={{
-          maxWidth: 600,
-          margin: "20px auto",
-          borderRadius: 8,
-          backgroundColor: "#142F43",
-        }}
-      >
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <Box px={3} py={3}>
-            <Grid container spacing={4}>
-              <Grid item xs={12}>
-                <Typography
-                  color="textSecondary"
-                  variant="body2"
-                  style={{ color: "#fff" }}
-                >
-                  *Fields required
-                </Typography>
-              </Grid>
-              <Grid item xs={12}>
-                <TextInput
-                  value="title"
-                  register={register}
-                  error={!!errors?.title}
-                  message={errors.title?.message ?? ""}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextInput
-                  multiline
-                  rows={5}
-                  value="description"
-                  register={register}
-                  error={!!errors?.description}
-                  message={errors.description?.message ?? ""}
-                />
-              </Grid>
-              <Grid
-                container
-                direction="row"
-                justifyContent="space-evenly"
-                alignItems="center"
+    <Paper elevation={14} style={style.paper}>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Box px={3} py={3}>
+          <Grid container spacing={4}>
+            <Grid item xs={12}>
+              <Typography
+                color="textSecondary"
+                variant="body2"
+                style={style.typography}
               >
-                <Grid item>
-                  <StyledButton
-                    hover="#cf890a"
-                    backgroundcolor="#ffa500"
-                    type="submit"
-                    ariaLabel="draft"
-                    label="draft"
-                    onClick={() => {
-                      setActionStatus("draft");
-                      getCampaignsData();
-                    }}
-                  />
-                </Grid>
-                <Grid item>
-                  <StyledButton
-                    hover="#286a2b"
-                    backgroundcolor="#2e7d32"
-                    label="send"
-                    ariaLabel="send"
-                    type="submit"
-                    onClick={() => {
-                      setActionStatus("sent");
-                      getCampaignsData();
-                    }}
-                  />
-                </Grid>
+                *Fields required
+              </Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <TextInput
+                value="title"
+                register={register}
+                error={!!errors?.title}
+                message={errors.title?.message ?? ""}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextInput
+                multiline
+                rows={5}
+                value="description"
+                register={register}
+                error={!!errors?.description}
+                message={errors.description?.message ?? ""}
+              />
+            </Grid>
+            <Grid
+              container
+              direction="row"
+              justifyContent="space-evenly"
+              alignItems="center"
+            >
+              <Grid item>
+                <StyledButton
+                  hover="#cf890a"
+                  backgroundcolor="#ffa500"
+                  type="submit"
+                  ariaLabel="draft"
+                  label="draft"
+                  onClick={() => {
+                    setActionStatus("draft");
+                    getCampaignsData();
+                  }}
+                />
+              </Grid>
+              <Grid item>
+                <StyledButton
+                  hover="#286a2b"
+                  backgroundcolor="#2e7d32"
+                  label="send"
+                  ariaLabel="send"
+                  type="submit"
+                  onClick={() => {
+                    setActionStatus("sent");
+                    getCampaignsData();
+                  }}
+                />
               </Grid>
             </Grid>
-          </Box>
-        </form>
-      </Paper>
-    </Container>
+          </Grid>
+        </Box>
+      </form>
+    </Paper>
   );
 };
 
