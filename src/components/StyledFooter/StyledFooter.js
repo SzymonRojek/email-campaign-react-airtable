f885230e16ad52a6b9d695010f43d3cbdad010b6
Szymon Rojek <sz.rojek@gmail.com>
2021-11-19 15:00:20 +0000

    add style objects, add zIndex to the footer

--- src/components/StyledFooter/StyledFooter.js
+++ src/components/StyledFooter/StyledFooter.js
@@ -3,24 +3,26 @@ import { AppBar, Typography } from "@material-ui/core";
 import { Link } from "@mui/material";
 import { BsGithub } from "react-icons/bs";
 
-const appBarStyles = {
-  position: "absolute",
-  bottom: 0,
-  backgroundColor: "#142f43",
-  color: "#ffffff8c",
-  minHeight: "5vh",
-};
-
-const iconStyles = {
-  fontSize: 25,
-  cursor: "pointer",
-  color: "orange",
-  zIndex: -1,
+const style = {
+  appBar: {
+    position: "absolute",
+    bottom: 0,
+    backgroundColor: "#142f43",
+    color: "#ffffff8c",
+    minHeight: "5vh",
+  },
+  icon: {
+    fontSize: 25,
+    cursor: "pointer",
+    color: "orange",
+    zIndex: -1,
+  },
+  footer: { marginBottom: 180, zIndex: 1 },
 };
 
 const StyledFooter = () => (
-  <footer style={{ marginBottom: 180 }}>
-    <AppBar position="static" color="primary" style={appBarStyles}>
+  <footer style={style.footer}>
+    <AppBar position="static" color="primary" style={style.appBar}>
       <Container maxWidth="md">
         <Box p={3}>
           <Grid
@@ -37,7 +39,7 @@ const StyledFooter = () => (
             </Grid>
             <Grid item xs={12}>
               <Link href="https://github.com/SzymonRojek" target="_blank">
-                <BsGithub style={iconStyles} />
+                <BsGithub style={style.icon} />
               </Link>
             </Grid>
           </Grid>
