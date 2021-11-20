6ce2940230f66e71e84d8af49e28eb2d0341c83a
Szymon Rojek <sz.rojek@gmail.com>
2021-11-20 17:37:12 +0000

    change export name

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -5,4 +5,4 @@ export { default as validationSubscriber } from "./validationSubscriber";
 export { default as validationCampaign } from "./validationCampaign";
 export { default as sortDataAlphabetically } from "./sortDataAlphabetically";
 export { default as capitalizeFirstLetter } from "./capitalizeFirstLetter";
-export { default as getLatestAddedSubscriber } from "./getLatestAddedSubscriber";
+export { default as getLatestAddedItem } from "./getLatestAddedItem";
