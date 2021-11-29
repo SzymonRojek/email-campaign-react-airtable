c4ea73098613a3bdb345203bfea62b010397efdd
Szymon Rojek <sz.rojek@gmail.com>
2021-11-29 15:18:00 +0000

    change validation scheme for the form - seperate validation when only basic data are submitted or full data are added

--- src/helpers/validationSubscriber.js
+++ src/helpers/validationSubscriber.js
@@ -1,6 +1,24 @@
 import * as Yup from "yup";
 
-const validationSubscriber = Yup.object().shape({
+const basicSubscriberInformation = {
+  name: Yup.string()
+    .required("Name is required")
+    .matches(/^[aA-zZ\s]+$/, "Only letters are required")
+    .min(3, "Name must be at least 6 characters")
+    .max(10, "Name must not exceed 10 characters"),
+  surname: Yup.string()
+    .required("Surname is required")
+    .matches(/^[aA-zZ\s]+$/, "Only letters are required")
+    .min(3, "Surname must be at least 3 characters")
+    .max(10, "Surname must not exceed 10 characters"),
+  profession: Yup.string()
+    .required("Surname is required")
+    .matches(/^[aA-zZ\s]+$/, "Only letters are required")
+    .min(3, "Surname must be at least 3 characters")
+    .max(10, "Surname must not exceed 10 characters"),
+};
+
+const allSubscriberInformation = {
   name: Yup.string()
     .required("Name is required")
     .matches(/^[aA-zZ\s]+$/, "Only letters are required")
@@ -19,6 +37,21 @@ const validationSubscriber = Yup.object().shape({
   email: Yup.string()
     .required("Email is required")
     .matches(/^([^.@]+)(\.[^.@]+)*@([^.@]+\.)+([^.@]+)$/, "Email is invalid"),
-});
+  salary: Yup.number()
+    .required("Salary is required")
+    .typeError("You must specify a number")
+    .min(3, "Must be at least 3 numbers"),
+  telephone: Yup.string()
+    .matches(
+      /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/,
+      "Phone is invalid"
+    )
+    .required("Phone is required"),
+};
+
+const validationSubscriber = (checked) =>
+  checked
+    ? Yup.object().shape(allSubscriberInformation)
+    : Yup.object().shape(basicSubscriberInformation);
 
 export default validationSubscriber;
