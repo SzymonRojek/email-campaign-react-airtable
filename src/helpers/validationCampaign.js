84a9cd88f79314ec75e700f6d815ccad85aa82d1
Szymon Rojek <sz.rojek@gmail.com>
2021-11-30 10:20:52 +0000

    edit validation

--- src/helpers/validationCampaign.js
+++ src/helpers/validationCampaign.js
@@ -2,13 +2,13 @@ import * as Yup from "yup";
 
 const validationCampaign = Yup.object().shape({
   title: Yup.string()
-    .required("Title is required")
-    .min(3, "Title must be at least 3 characters")
-    .max(20, "Title must not exceed 20 characters"),
+    .required("title is required")
+    .min(3, "must be at least 3 characters")
+    .max(20, "must not exceed 20 characters"),
   description: Yup.string()
-    .required("Description is required")
-    .min(3, "Description must be at least 3 characters")
-    .max(60, "Description must not exceed 60 characters"),
+    .required("description is required")
+    .min(3, "must be at least 3 characters")
+    .max(60, "must not exceed 60 characters"),
 });
 
 export default validationCampaign;
--- src/helpers/validationSubscriber.js
+++ src/helpers/validationSubscriber.js
@@ -2,51 +2,54 @@ import * as Yup from "yup";
 
 const basicSubscriberInformation = {
   name: Yup.string()
-    .required("Name is required")
-    .matches(/^[aA-zZ\s]+$/, "Only letters are required")
-    .min(3, "Name must be at least 6 characters")
-    .max(10, "Name must not exceed 10 characters"),
+    .required("name is required")
+    .matches(/^[aA-zZ\s]+$/, "only letters are required")
+    .min(3, "must be at least 6 characters")
+    .max(10, "must not exceed 10 characters"),
   surname: Yup.string()
-    .required("Surname is required")
-    .matches(/^[aA-zZ\s]+$/, "Only letters are required")
-    .min(3, "Surname must be at least 3 characters")
-    .max(10, "Surname must not exceed 10 characters"),
+    .required("surname is required")
+    .matches(/^[aA-zZ\s]+$/, "only letters are required")
+    .min(3, "must be at least 3 characters")
+    .max(10, "must not exceed 10 characters"),
   profession: Yup.string()
-    .required("Surname is required")
-    .matches(/^[aA-zZ\s]+$/, "Only letters are required")
-    .min(3, "Surname must be at least 3 characters")
-    .max(10, "Surname must not exceed 10 characters"),
+    .required("is required")
+    .matches(/^[aA-zZ\s]+$/, "only letters are required")
+    .min(3, "must be at least 3 characters")
+    .max(10, "must not exceed 10 characters"),
 };
+
+const status = ["pending", "blocked", "active"];
 
 const allSubscriberInformation = {
   name: Yup.string()
-    .required("Name is required")
-    .matches(/^[aA-zZ\s]+$/, "Only letters are required")
-    .min(3, "Name must be at least 6 characters")
-    .max(10, "Name must not exceed 10 characters"),
+    .required("name is required")
+    .matches(/^[aA-zZ\s]+$/, "only letters are required")
+    .min(3, "must be at least 6 characters")
+    .max(10, "must not exceed 10 characters"),
   surname: Yup.string()
-    .required("Surname is required")
-    .matches(/^[aA-zZ\s]+$/, "Only letters are required")
-    .min(3, "Surname must be at least 3 characters")
-    .max(10, "Surname must not exceed 10 characters"),
+    .required("surname is required")
+    .matches(/^[aA-zZ\s]+$/, "only letters are required")
+    .min(3, "must be at least 3 characters")
+    .max(10, "must not exceed 10 characters"),
   profession: Yup.string()
-    .required("Surname is required")
-    .matches(/^[aA-zZ\s]+$/, "Only letters are required")
-    .min(3, "Surname must be at least 3 characters")
-    .max(10, "Surname must not exceed 10 characters"),
+    .required("profession is required")
+    .matches(/^[aA-zZ\s]+$/, "only letters are required")
+    .min(3, "must be at least 3 characters")
+    .max(10, "must not exceed 10 characters"),
+  status: Yup.string().required("Status is required").oneOf(status),
   email: Yup.string()
-    .required("Email is required")
-    .matches(/^([^.@]+)(\.[^.@]+)*@([^.@]+\.)+([^.@]+)$/, "Email is invalid"),
+    .required("email is required")
+    .matches(/^([^.@]+)(\.[^.@]+)*@([^.@]+\.)+([^.@]+)$/, "email is invalid"),
   salary: Yup.number()
-    .required("Salary is required")
-    .typeError("You must specify a number")
-    .min(3, "Must be at least 3 numbers"),
+    .required("salary is required")
+    .typeError("must specify a number")
+    .min(3, "must be at least 3 numbers"),
   telephone: Yup.string()
     .matches(
       /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/,
-      "Phone is invalid"
+      "phone is invalid"
     )
-    .required("Phone is required"),
+    .required("phone is required"),
 };
 
 const validationSubscriber = (checked) =>
