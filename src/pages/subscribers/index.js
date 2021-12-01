f20c37965b91279cf8e8f0c6ed5531e46c34a776
Szymon Rojek <sz.rojek@gmail.com>
2021-12-01 22:59:26 +0000

    rename FilteredSubscribersList to SubscribersStatusPage

--- src/pages/subscribers/index.js
+++ src/pages/subscribers/index.js
@@ -1,4 +1,4 @@
 export { default as SubscribersPage } from "./SubscribersPage";
-export { default as FilteredSubscribersList } from "./FilteredSubscribersList";
+export { default as SubscribersStatusPage } from "./SubscribersStatusPage";
 export { default as SubscriberDetails } from "./SubscriberDetails";
 export { default as NewSubscriber } from "./NewSubscriber";
