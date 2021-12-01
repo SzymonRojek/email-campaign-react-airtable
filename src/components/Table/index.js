0314c3d2d6035f7545f262d490686fde84065552
Szymon Rojek <sz.rojek@gmail.com>
2021-12-01 23:16:22 +0000

    remove export

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -1,4 +1,3 @@
 export { default as ContainerTable } from "./ContainerTable";
 export { default as HeadTable } from "./HeadTable";
 export { default as BodyTable } from "./BodyTable";
-export { default as RowSubscriber } from "../SubscriberTableRow/SubscriberTableRow";
