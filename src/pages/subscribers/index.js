5d4d0d65a47a3508be9a0539212d40885ccc5928
Szymon Rojek <sz.rojek@gmail.com>
2021-12-01 23:21:51 +0000

    change export

--- src/pages/subscribers/index.js
+++ src/pages/subscribers/index.js
@@ -1,4 +1,4 @@
 export { default as SubscribersPage } from "./SubscribersPage";
 export { default as SubscribersStatusPage } from "./SubscribersStatusPage";
-export { default as SubscriberDetails } from "./SubscriberDetails";
+export { default as SubscriberDetailsPage } from "./SubscriberDetailsPage";
 export { default as NewSubscriber } from "./NewSubscriber";
