f1c9daee89cc99f428475775c04f8f29d8822e31
Szymon Rojek <sz.rojek@gmail.com>
2021-12-03 15:19:53 +0000

    create generic hook

--- src/useFetchData.js
+++ src/useFetchData.js
@@ -1,38 +1,26 @@
 import { useState, useEffect } from "react";
 
 import api from "./api";
-import { sortDataAlphabetically, getLatestAddedItem } from "./helpers";
 
-export function useFetchData(endpointOne, endpointSecond) {
-  const [subscribersData, setSubscribersData] = useState({
+export function useFetchData(endpoint) {
+  const [data, setData] = useState({
     status: "loading",
     data: null,
-    latestAddedItem: null,
   });
-  const [campaignsData, setCampaignsData] = useState({
-    status: "loading",
-    data: null,
-    latestAddedItem: null,
-  });
-  const subscribersUrl = "/subscribers";
-  const campaignsUrl = "/campaigns";
 
-  const getSubscribersData = async () => {
-    if (endpointOne !== subscribersUrl) setSubscribersData({ status: "error" });
-
-    if (endpointOne === subscribersUrl) {
+  const getData = async () => {
+    if (!endpoint) {
+      setData({ status: "error" });
+    } else {
       try {
-        const { records } = await api.get(endpointOne);
+        const { records } = await api.get(endpoint);
 
-        sortDataAlphabetically(records);
-
-        setSubscribersData({
+        setData({
           status: "success",
           data: records,
-          latestAddedItem: getLatestAddedItem(records),
         });
       } catch (error) {
-        setSubscribersData({
+        setData({
           status: "error",
         });
       }
@@ -40,47 +28,96 @@ export function useFetchData(endpointOne, endpointSecond) {
   };
 
   useEffect(() => {
-    const timeId = setInterval(() => getSubscribersData(), 3_000);
-
-    return () => clearInterval(timeId);
-  }, []);
-
-  const getCampaignsData = async () => {
-    if (endpointSecond !== campaignsUrl) setCampaignsData({ status: "error" });
-
-    if (endpointSecond === campaignsUrl) {
-      try {
-        const { records } = await api.get(endpointSecond);
-
-        sortDataAlphabetically(records);
-
-        setCampaignsData({
-          status: "success",
-          data: records,
-          latestAddedItem: getLatestAddedItem(records),
-        });
-      } catch (error) {
-        setCampaignsData({
-          status: "error",
-        });
-      }
-    }
-  };
-
-  useEffect(() => {
-    const timeId = setTimeout(() => getCampaignsData(), 3_000);
+    const timeId = setTimeout(getData, 3_000);
 
     return () => clearTimeout(timeId);
   }, []);
 
-  return [
-    {
-      subscribersData,
-      campaignsData,
-      setSubscribersData,
-      setCampaignsData,
-    },
-    getSubscribersData,
-    getCampaignsData,
-  ];
+  return {
+    data,
+    setData,
+    getData,
+  };
 }
+
+// export function useFetchData(endpointOne, endpointSecond) {
+//   const [subscribersData, setSubscribersData] = useState({
+//     status: "loading",
+//     data: null,
+//     latestAddedItem: null,
+//   });
+//   const [campaignsData, setCampaignsData] = useState({
+//     status: "loading",
+//     data: null,
+//     latestAddedItem: null,
+//   });
+//   const subscribersUrl = "/subscribers";
+//   const campaignsUrl = "/campaigns";
+
+//   const getSubscribersData = async () => {
+//     if (endpointOne !== subscribersUrl) setSubscribersData({ status: "error" });
+
+//     if (endpointOne === subscribersUrl) {
+//       try {
+//         const { records } = await api.get(endpointOne);
+
+//         sortDataAlphabetically(records);
+
+//         setSubscribersData({
+//           status: "success",
+//           data: records,
+//           latestAddedItem: getLatestAddedItem(records),
+//         });
+//       } catch (error) {
+//         setSubscribersData({
+//           status: "error",
+//         });
+//       }
+//     }
+//   };
+
+//   useEffect(() => {
+//     const timeId = setInterval(() => getSubscribersData(), 3_000);
+
+//     return () => clearInterval(timeId);
+//   }, []);
+
+//   const getCampaignsData = async () => {
+//     if (endpointSecond !== campaignsUrl) setCampaignsData({ status: "error" });
+
+//     if (endpointSecond === campaignsUrl) {
+//       try {
+//         const { records } = await api.get(endpointSecond);
+
+//         sortDataAlphabetically(records);
+
+//         setCampaignsData({
+//           status: "success",
+//           data: records,
+//           latestAddedItem: getLatestAddedItem(records),
+//         });
+//       } catch (error) {
+//         setCampaignsData({
+//           status: "error",
+//         });
+//       }
+//     }
+//   };
+
+//   useEffect(() => {
+//     const timeId = setTimeout(() => getCampaignsData(), 3_000);
+
+//     return () => clearTimeout(timeId);
+//   }, []);
+
+//   return [
+//     {
+//       subscribersData,
+//       campaignsData,
+//       setSubscribersData,
+//       setCampaignsData,
+//     },
+//     getSubscribersData,
+//     getCampaignsData,
+//   ];
+// }
