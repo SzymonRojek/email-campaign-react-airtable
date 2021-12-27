7acbc03c1fbeac0d9e86a5401ef25dc662450d08
Szymon Rojek <sz.rojek@gmail.com>
2021-12-27 19:24:27 +0000

    remove useStyles classes, remove props from th table container

--- client/src/components/Table/ContainerTable.js
+++ client/src/components/Table/ContainerTable.js
@@ -1,38 +1,22 @@
 import PropTypes from "prop-types";
-import { makeStyles } from "@material-ui/core/styles";
-import { Table, TableContainer, Paper } from "@material-ui/core";
+import { Table, TableContainer } from "@material-ui/core";
 
 const style = {
   textAlign: "left",
-  marginBottom: -20,
-  color: "#142F43",
+  paddingLeft: 20,
   letterSpacing: 2,
 };
 
-const useStyles = makeStyles((theme) => ({
-  tableContainer: {
-    borderRadius: 15,
-    margin: "50px auto",
-  },
-}));
-
-const ContainerTable = ({ subHeading, children }) => {
-  const classes = useStyles();
-  return (
-    <>
-      <header>
-        <h3 style={style}>{subHeading}</h3>
-      </header>
-      <TableContainer
-        component={Paper}
-        className={classes.tableContainer}
-        elevation={4}
-      >
-        <Table aria-label="subscribers table">{children}</Table>
-      </TableContainer>
-    </>
-  );
-};
+const ContainerTable = ({ subHeading, children }) => (
+  <>
+    <header>
+      <h3 style={style}>{subHeading}</h3>
+    </header>
+    <TableContainer>
+      <Table aria-label="subscribers table">{children}</Table>
+    </TableContainer>
+  </>
+);
 
 ContainerTable.propTypes = {
   children: PropTypes.array.isRequired,
