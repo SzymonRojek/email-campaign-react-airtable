aa8e0bfae937d1026004170e373b232c8144d87f
Szymon Rojek <sz.rojek@gmail.com>
2021-12-27 19:25:03 +0000

    add width to the cell

--- client/src/components/Table/HeadTable.js
+++ client/src/components/Table/HeadTable.js
@@ -4,6 +4,7 @@ import { TableCell, TableHead, TableRow } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
   tableHeaderCell: {
+    minWidth: 100,
     fontWeight: "bold",
     backgroundColor: "#142f43",
     color: theme.palette.getContrastText(theme.palette.primary.dark),
