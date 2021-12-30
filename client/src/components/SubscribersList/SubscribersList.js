ecc878e042ad5804b42be34f9a2640c4b2a2e703
Szymon Rojek <sz.rojek@gmail.com>
2021-12-30 18:02:26 +0000

    remove return, remove state variable activatedTablePage and setActiveTablePage, remove some props, rename subsribersData to data inside renderData prop, add actualPage to the renderData and pass actualPage to the SusbcriberTableRow

--- client/src/components/SubscribersList/SubscribersList.js
+++ client/src/components/SubscribersList/SubscribersList.js
@@ -1,11 +1,10 @@
 import PropTypes from "prop-types";
-import { useState } from "react";
 
-import { ContainerTable, HeadTable, BodyTable } from "../Table";
-import { SubscriberTableRow } from "../SubscriberTableRow";
-import CustomPaginator from "../PaginationPackage/CustomPaginator";
+import { ContainerTable, HeadTable, BodyTable } from "components/Table";
+import { SubscriberTableRow } from "components/SubscriberTableRow";
+import CustomPaginator from "components/PaginationPackage/CustomPaginator";
 
-const dataPerPage = 6;
+const dataPerPage = 5;
 
 const SubscribersList = ({
   subHeading,
@@ -15,47 +14,39 @@ const SubscribersList = ({
   setSelectedData,
   setContentPopup,
   setOpenConfirmPopup,
-}) => {
-  const [activatedTablePage, setActiveTablePage] = useState(1);
-
-  return (
-    <CustomPaginator
-      passedData={passedData}
-      dataPerPage={dataPerPage}
-      disableDuration={400}
-      switcherDisplay="both" //top/bottom/both/none(or another string)
-      disableArrows={false} //true/false
-      disableDigits={false} //true/false
-      setActiveTablePage={setActiveTablePage}
-      renderData={(subscribersData) => (
-        <ContainerTable subHeading={subHeading}>
-          <HeadTable dataHeadTable={dataHeadTable} />
-          <BodyTable>
-            {subscribersData &&
-              subscribersData.map((subscriber, index) => (
-                <SubscriberTableRow
-                  key={`id-${subscriber.id}`}
-                  subscriber={{ ...subscriber, group: "subscribers" }}
-                  index={index}
-                  dataPerPage={dataPerPage}
-                  activatedTablePage={activatedTablePage}
-                  setSelectedData={setSelectedData}
-                  handleSubscriberDetails={handleSubscriberDetails}
-                  setContentPopup={setContentPopup}
-                  setOpenConfirmPopup={setOpenConfirmPopup}
-                />
-              ))}
-          </BodyTable>
-        </ContainerTable>
-      )}
-    />
-  );
-};
+}) => (
+  <CustomPaginator
+    passedData={passedData}
+    dataPerPage={dataPerPage}
+    disableDuration={400}
+    disableArrows={false}
+    disableDigits={false}
+    renderData={(data, actualPage) => (
+      <ContainerTable subHeading={subHeading}>
+        <HeadTable dataHeadTable={dataHeadTable} />
+        <BodyTable>
+          {data.map((subscriber, index) => (
+            <SubscriberTableRow
+              key={`id-${subscriber.id}`}
+              subscriber={{ ...subscriber, group: "subscribers" }}
+              index={index}
+              actualPage={actualPage}
+              dataPerPage={dataPerPage}
+              setSelectedData={setSelectedData}
+              handleSubscriberDetails={handleSubscriberDetails}
+              setContentPopup={setContentPopup}
+              setOpenConfirmPopup={setOpenConfirmPopup}
+            />
+          ))}
+        </BodyTable>
+      </ContainerTable>
+    )}
+  />
+);
 
 SubscribersList.propTypes = {
   subHeading: PropTypes.string.isRequired,
   dataHeadTable: PropTypes.array.isRequired,
-  subscribersData: PropTypes.array.isRequired,
   handleSubscriberDetails: PropTypes.func.isRequired,
   setSelectedData: PropTypes.func.isRequired,
   setContentPopup: PropTypes.func.isRequired,
@@ -63,25 +54,3 @@ SubscribersList.propTypes = {
 };
 
 export default SubscribersList;
-
-{
-  /* <ContainerTable subHeading={subHeading}>
-<HeadTable dataHeadTable={dataHeadTable} />
-<BodyTable>
-  {subscribersData &&
-    subscribersData.map((subscriber, index) => (
-      <SubscriberTableRow
-        key={`id-${subscriber.id}`}
-        subscriber={{ ...subscriber, group: "subscribers" }}
-        index={index}
-        dataPerPage={dataPerPage}
-        activatedTablePage={activatedTablePage}
-        setSelectedData={setSelectedData}
-        handleSubscriberDetails={handleSubscriberDetails}
-        setContentPopup={setContentPopup}
-        setOpenConfirmPopup={setOpenConfirmPopup}
-      />
-    ))}
-</BodyTable>
-</ContainerTable> */
-}
