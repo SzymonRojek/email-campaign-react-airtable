b3142dbba20eec1490f69e92465e9548328b926b
Szymon Rojek <sz.rojek@gmail.com>
2022-01-02 16:24:59 +0000

    add new export

--- client/src/helpers/index.js
+++ client/src/helpers/index.js
@@ -6,3 +6,4 @@ export { default as validationCampaign } from "./validationCampaign";
 export { default as sortDataAlphabetically } from "./sortDataAlphabetically";
 export { default as capitalizeFirstLetter } from "./capitalizeFirstLetter";
 export { default as getLatestAddedItem } from "./getLatestAddedItem";
+export { default as getFilteredDataByStatus } from "./getFilteredDataByStatus";
