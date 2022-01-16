5dd81116104e08c16185ec44c1829fd2e0d3a86d
Szymon Rojek <sz.rojek@gmail.com>
2022-01-16 22:19:11 +0000

    import hook useStateWithLocalStorage, add variable defaultLogIn and add ternatry operator - display AppContainer only when loginValue is equal to defaultLogIn

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,21 @@
 import AppContainer from "./AppContainer";
 import "./App.css";
+import LoginForm from "./components/LoginForm/LoginForm";
+import { useStateWithLocalStorage } from "./useStateWithLocalStorage";
 
-const App = () => (
-  <div className="page-container">
-    <AppContainer />
-  </div>
-);
+const App = () => {
+  const [loginValue, setLoginValue] = useStateWithLocalStorage("login");
+  const defaultLogIn = "admin";
+
+  return (
+    <div className="page-container">
+      {loginValue === defaultLogIn ? (
+        <AppContainer setLoginValue={setLoginValue} />
+      ) : (
+        <LoginForm setValue={setLoginValue} />
+      )}
+    </div>
+  );
+};
 
 export default App;
