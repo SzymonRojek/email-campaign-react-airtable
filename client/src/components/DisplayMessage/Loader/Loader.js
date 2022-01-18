970ce91593552a92111925bc72e35aa470421a39
Szymon Rojek <sz.rojek@gmail.com>
2022-01-18 21:55:30 +0000

    edit styles, add additional div

--- client/src/components/DisplayMessage/Loader/Loader.js
+++ client/src/components/DisplayMessage/Loader/Loader.js
@@ -3,15 +3,17 @@ import PropTypes from "prop-types";
 import "./styles.css";
 
 const Loader = ({ title }) => (
-  <div className="loader-container">
-    <div className="load"></div>
+  <div className="main-container">
+    <div className="loader-container">
+      <div className="load"></div>
 
-    <div className="text-container">
-      <p className="loader-text">{title} Data</p>
+      <div className="text-container">
+        <p className="loader-text">{title} Data</p>
 
-      <div className="dot-container">
-        <p className="loader-text">loading</p>
-        <div className="dot-flashing"></div>
+        <div className="dot-container">
+          <p className="loader-text">loading</p>
+          <div className="dot-flashing"></div>
+        </div>
       </div>
     </div>
   </div>
