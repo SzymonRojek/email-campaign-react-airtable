c462365b8889868f9a76fdae80bf537326c078b1
Szymon Rojek <sz.rojek@gmail.com>
2022-01-19 18:12:20 +0000

    add useStyles, add useForm, download component textInputController

--- client/src/components/LoginForm/LoginForm.js
+++ client/src/components/LoginForm/LoginForm.js
@@ -1,43 +1,100 @@
-import { useState } from "react";
-import LogFormButton from "../LogFormButton/LogFormButton";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { Paper, Box, Grid, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 
-const LoginForm = ({ setValue }) => {
-  const [inputValue, setInputValue] = useState("");
+import { TextInputController } from "../Inputs";
+import { LogFormButton } from "../LogFormButton";
+import validationLogin from "../../helpers/validationLogin";
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
+const useStyles = makeStyles(() => ({
+  paper: {
+    maxWidth: 600,
+    margin: "120px auto",
+    borderRadius: 8,
+    backgroundColor: "#142F43",
 
-    setValue(() => inputValue);
-  };
+    "@media (min-width:960px)": {
+      marginTop: 240,
+    },
+  },
+}));
 
-  const onChange = (event) => {
-    setInputValue(event.target.value);
+const style = {
+  typography: { color: "orange", letterSpacing: 2, wordSpacing: 3 },
+  heading: {
+    color: "orange",
+    textAlign: "center",
+    letterSpacing: 2,
+    wordSpacing: 3,
+  },
+};
+
+const LoginForm = ({ setLoginValue }) => {
+  const classes = useStyles();
+
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(validationLogin),
+  });
+
+  const onSubmit = (data) => {
+    if (data.password === data.confirmPassword) {
+      setLoginValue(true);
+    }
   };
 
   return (
-    <form
-      onSubmit={handleSubmit}
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        maxWidth: 300,
-        margin: "20px auto",
-        padding: 60,
-        backgroundColor: "#142f43",
-        color: "orange",
-      }}
-    >
-      <h1>Email Campaign</h1>
+    <Paper elevation={14} className={classes.paper}>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Box px={3} py={3}>
+          <Grid container spacing={4}>
+            <Grid item xs={12}>
+              <Typography
+                color="textSecondary"
+                variant="body2"
+                style={style.typography}
+              >
+                *Fields required
+              </Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <h1 color="textSecondary" variant="h1" style={style.heading}>
+                Email Campaign
+              </h1>
+            </Grid>
+            <Grid item xs={12}>
+              <TextInputController
+                control={control}
+                name="password"
+                error={!!errors.password}
+                message={errors.password?.message ?? ""}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextInputController
+                control={control}
+                name="confirmPassword"
+                error={!!errors.confirmPassword}
+                message={errors.confirmPassword?.message ?? ""}
+              />
+            </Grid>
 
-      <input
-        placeholder="type admin"
-        value={inputValue}
-        type="text"
-        onChange={onChange}
-        style={{ padding: 10, marginTop: 20 }}
-      />
-      <LogFormButton label="log in" style={{ margin: "50px auto" }} />
-    </form>
+            <Grid
+              container
+              direction="row"
+              justifyContent="space-evenly"
+              alignItems="center"
+            >
+              <LogFormButton label="log in" style={{ margin: "50px auto" }} />
+            </Grid>
+          </Grid>
+        </Box>
+      </form>
+    </Paper>
   );
 };
 
