bdbbaccf76103efd8f1a14646eb078b38a9eb9b7
Szymon Rojek <sz.rojek@gmail.com>
2022-01-19 23:36:02 +0000

    add textshadow to the heading

--- client/src/components/StyledHeading/StyledHeading.js
+++ client/src/components/StyledHeading/StyledHeading.js
@@ -1,13 +1,14 @@
 import PropTypes from "prop-types";
 
 const headingStyles = {
-  textAlign: "center",
-  paddingTop: 35,
   marginBottom: 40,
+  paddingTop: 35,
   color: "#142F43",
   letterSpacing: 2,
   wordSpacing: 15,
+  textAlign: "center",
   textTransform: "uppercase",
+  textShadow: "-2px 2px white",
 };
 
 const StyledHeading = ({ label }) => <h1 style={headingStyles}>{label}</h1>;
