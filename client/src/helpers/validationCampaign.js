8e3568317984dc314371a8c5b2e392a548583c3d
Szymon Rojek <sz.rojek@gmail.com>
2022-01-19 18:18:55 +0000

    add trim and change some max characters numbers

--- client/src/helpers/validationCampaign.js
+++ client/src/helpers/validationCampaign.js
@@ -3,12 +3,14 @@ import * as Yup from "yup";
 const validationCampaign = Yup.object().shape({
   title: Yup.string()
     .required("title is required")
+    .trim()
     .min(3, "must be at least 3 characters")
-    .max(20, "must not exceed 20 characters"),
+    .max(30, "must not exceed 30 characters"),
   description: Yup.string()
     .required("description is required")
+    .trim()
     .min(3, "must be at least 3 characters")
-    .max(60, "must not exceed 60 characters"),
+    .max(100, "must not exceed 100 characters"),
 });
 
 export default validationCampaign;
--- client/src/helpers/validationSubscriber.js
+++ client/src/helpers/validationSubscriber.js
@@ -3,16 +3,19 @@ import * as Yup from "yup";
 const basicSubscriberInformation = {
   name: Yup.string()
     .required("name is required")
+    .trim()
     .matches(/^[aA-zZ\s]+$/, "only letters are required")
     .min(3, "must be at least 6 characters")
     .max(10, "must not exceed 10 characters"),
   surname: Yup.string()
     .required("surname is required")
+    .trim()
     .matches(/^[aA-zZ\s]+$/, "only letters are required")
     .min(3, "must be at least 3 characters")
     .max(10, "must not exceed 10 characters"),
   email: Yup.string()
     .required("email is required")
+    .trim()
     .matches(/^([^.@]+)(\.[^.@]+)*@([^.@]+\.)+([^.@]+)$/, "email is invalid"),
 };
 
@@ -21,28 +24,34 @@ const status = ["pending", "blocked", "active"];
 const allSubscriberInformation = {
   name: Yup.string()
     .required("name is required")
+    .trim()
     .matches(/^[aA-zZ\s]+$/, "only letters are required")
     .min(3, "must be at least 6 characters")
     .max(10, "must not exceed 10 characters"),
   surname: Yup.string()
     .required("surname is required")
+    .trim()
     .matches(/^[aA-zZ\s]+$/, "only letters are required")
     .min(3, "must be at least 3 characters")
     .max(10, "must not exceed 10 characters"),
   profession: Yup.string()
     .required("profession is required")
+    .trim()
     .matches(/^[aA-zZ\s]+$/, "only letters are required")
     .min(3, "must be at least 3 characters")
     .max(10, "must not exceed 10 characters"),
-  status: Yup.string().required("status is required").oneOf(status),
+  status: Yup.string().required("status is required").trim().oneOf(status),
   email: Yup.string()
     .required("email is required")
+    .trim()
     .matches(/^([^.@]+)(\.[^.@]+)*@([^.@]+\.)+([^.@]+)$/, "email is invalid"),
   salary: Yup.number()
     .required("salary is required")
     .typeError("must specify a number")
     .min(3, "must be at least 3 numbers"),
   telephone: Yup.string()
+    .required("telephone is required")
+    .trim()
     .matches(
       /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/,
       "phone is invalid"
