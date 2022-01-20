1ee4f09dd89f745476a93694847d3e612d115f0f
Szymon Rojek <sz.rojek@gmail.com>
2022-01-20 13:14:30 +0000

    edit validation

--- client/src/helpers/validationLogin.js
+++ client/src/helpers/validationLogin.js
@@ -2,14 +2,14 @@ import * as Yup from "yup";
 
 const validationLogin = Yup.object().shape({
   password: Yup.string()
-    .required("Please enter your password")
+    .required("please enter your password")
     .lowercase()
-    .matches(/admin/, "Password is not correct"),
+    .matches(/admin/, "password is not correct"),
   confirmPassword: Yup.string()
-    .required("Please confirm your password")
+    .required("please confirm your password")
     .trim()
     .lowercase()
-    .oneOf([Yup.ref("password"), null], "Passwords don't match."),
+    .oneOf([Yup.ref("password"), null], "passwords don't match."),
 });
 
 export default validationLogin;
