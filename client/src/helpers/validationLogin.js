c397c01796e7df26348c45e236fd7887b7585702
Szymon Rojek <sz.rojek@gmail.com>
2022-01-20 00:55:30 +0000

    add lowerCase to login validation

--- client/src/helpers/validationLogin.js
+++ client/src/helpers/validationLogin.js
@@ -3,10 +3,12 @@ import * as Yup from "yup";
 const validationLogin = Yup.object().shape({
   password: Yup.string()
     .required("Please enter your password")
+    .lowercase()
     .matches(/admin/, "Password is not correct"),
   confirmPassword: Yup.string()
     .required("Please confirm your password")
     .trim()
+    .lowercase()
     .oneOf([Yup.ref("password"), null], "Passwords don't match."),
 });
 
