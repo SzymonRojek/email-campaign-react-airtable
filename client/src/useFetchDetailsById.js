eed7d403759251e83f85ba4297a5f7079fd302be
Szymon Rojek <sz.rojek@gmail.com>
2022-01-24 14:18:33 +0000

    remove if statement, add id parameter and add id to the dependency array

--- client/src/useFetchDetailsById.js
+++ client/src/useFetchDetailsById.js
@@ -2,28 +2,24 @@ import { useEffect, useState } from "react";
 
 import api from "./api";
 
-export const useFetchDetailsById = (endpoint) => {
+export const useFetchDetailsById = (endpoint, id) => {
   const [itemData, setItemData] = useState({
     status: "loading",
     data: null,
   });
 
   const fetchData = async () => {
-    if (!endpoint) {
-      setItemData({ status: "error" });
-    } else {
-      try {
-        const data = await api.get(endpoint);
+    try {
+      const data = await api.get(`${endpoint}/${id}`);
 
-        setItemData({
-          status: "success",
-          data,
-        });
-      } catch (error) {
-        setItemData({
-          status: "error",
-        });
-      }
+      setItemData({
+        status: "success",
+        data,
+      });
+    } catch (error) {
+      setItemData({
+        status: "error",
+      });
     }
   };
 
@@ -31,7 +27,7 @@ export const useFetchDetailsById = (endpoint) => {
     const delayGetData = setTimeout(fetchData, 2_000);
 
     return () => clearTimeout(delayGetData);
-  }, []);
+  }, [id]);
 
   return { itemData, setItemData };
 };
