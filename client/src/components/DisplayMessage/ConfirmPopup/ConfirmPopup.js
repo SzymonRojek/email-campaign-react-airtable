5b92baf8e9c69e9812fcd325d685f748bc0487de
Szymon Rojek <sz.rojek@gmail.com>
2022-01-28 17:26:41 +0000

    add useStyles, remove DialogTitle, DialogContent, add div container

--- client/src/components/DisplayMessage/ConfirmPopup/ConfirmPopup.js
+++ client/src/components/DisplayMessage/ConfirmPopup/ConfirmPopup.js
@@ -1,61 +1,63 @@
 import PropTypes from "prop-types";
-import { Dialog, DialogContent, DialogTitle } from "@mui/material";
+import { Dialog, DialogContent } from "@mui/material";
 import Button from "@mui/material/Button";
+import { makeStyles } from "@material-ui/core/styles";
+
 import { usePopup } from "popupContext.js";
 
 const styles = {
+  containerText: { padding: "35px 25px" },
   dialogContent: {
     display: "flex",
     justifyContent: "space-around",
     padding: "30px 0 25px 0",
   },
-  dialogTitleTwo: { textAlign: "center" },
-  span: { color: "crimson" },
-  button: { color: "white" },
+  button: { color: "white", fontWeight: "bold" },
 };
 
+const useStyles = makeStyles((theme) => ({
+  [theme.breakpoints.down("xs")]: {
+    addiotnalText: {
+      fontSize: 18,
+      textAlign: "center",
+      fontWeight: "bold",
+      margin: 0,
+    },
+    text: { fontSize: 18, textAlign: "center", padding: "10px 0", margin: 0 },
+  },
+  [theme.breakpoints.up("sm")]: {
+    additionalText: {
+      fontSize: 25,
+      textAlign: "center",
+      fontWeight: "bold",
+      padding: 10,
+      margin: 0,
+    },
+    text: { fontSize: 25, textAlign: "center", padding: "10px 0", margin: 0 },
+    paper: {
+      maxWidth: 500,
+    },
+  },
+}));
+
 const ConfirmPopup = () => {
+  const classes = useStyles();
   const { isOpenConfirmPopup, text, closeConfirmPopup, actionPopup } =
     usePopup();
 
   return (
-    <Dialog open={isOpenConfirmPopup} maxWidth="sm">
-      {text.titleOne && (
-        <>
-          <DialogTitle>
-            <p
-              style={{
-                color: "green",
-                textAlign: "center",
-                fontWeight: "bold",
-              }}
-            >
-              {text.titleOne}
-            </p>
-          </DialogTitle>
-          <DialogContent>
-            <DialogContent dividers>
-              <p style={{ fontSize: 18, textAlign: "center" }}>
-                {text.description}
-              </p>
+    <Dialog open={isOpenConfirmPopup} classes={{ paper: classes.paper }}>
+      <div style={styles.containerText}>
+        <p className={classes.additionalText}>{text.additionalText}</p>
+        {text.title && <p className={classes.text}>{text.title}</p>}
 
-              <p style={{ fontSize: 18, textAlign: "center" }}>
-                {text.additionalText}
-              </p>
-            </DialogContent>
-          </DialogContent>
-        </>
-      )}
-      <DialogTitle style={styles.dialogTitleTwo}>
-        <p>
-          {text.titleTwo}
-          <span style={{ color: "crimson" }}> {text.titleItem} </span> ?
-        </p>
-      </DialogTitle>
+        <p className={classes.text}>{text.question}</p>
+      </div>
       <DialogContent dividers style={styles.dialogContent}>
         <Button
           variant="contained"
           color="error"
+          style={styles.button}
           onClick={() => {
             actionPopup.change();
             closeConfirmPopup();
