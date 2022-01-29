6bfc1c33963c5a0d21d0d115e2bbc392ad7fcd30
Szymon Rojek <sz.rojek@gmail.com>
2022-01-29 23:04:32 +0000

    modify StyledCheckbox component

--- client/src/components/Inputs/StyledCheckbox/StyledCheckbox.js
+++ client/src/components/Inputs/StyledCheckbox/StyledCheckbox.js
@@ -1,12 +1,65 @@
-import { FormControlLabel, Checkbox } from "@mui/material";
+import { FormControl, Checkbox } from "@mui/material";
+import { Controller } from "react-hook-form";
+import { Typography } from "@material-ui/core";
 
-const StyledCheckbox = ({ sx, style, label, onChange, defaultValue }) => (
-  <FormControlLabel
-    control={
-      <Checkbox sx={sx} defaultValue={defaultValue} onChange={onChange} />
-    }
-    label={<span style={style}>{label}</span>}
-  />
+const styles = {
+  mainContainer: { display: "flex", flexDirection: "column" },
+  checkboxContainer: {
+    display: "flex",
+    justifyContent: "left",
+    alignItems: "center",
+  },
+  checkbox: {
+    transform: "scale(1.1)",
+    color: "orange",
+    "&.Mui-checked": {
+      color: "orange",
+    },
+  },
+  label: {
+    fontSize: "1rem",
+    color: "orange",
+    paddingLeft: 8,
+    letterSpacing: 1,
+  },
+  error: { color: "crimson", paddingTop: 4, letterSpacing: 1 },
+};
+
+const StyledCheckbox = ({
+  control,
+  name,
+  defaultValue,
+  label,
+  error,
+  message,
+}) => (
+  <FormControl style={styles.mainContainer}>
+    <div style={styles.checkboxContainer}>
+      <Controller
+        control={control}
+        name={name}
+        defaultValue={defaultValue}
+        rules={{ required: true }}
+        error={error}
+        render={({ field: { ref, value, ...field } }) => (
+          <Checkbox
+            {...field}
+            checked={value}
+            inputRef={ref}
+            sx={styles.checkbox}
+          />
+        )}
+      />
+
+      <Typography variant="inherit" style={styles.label}>
+        {label}
+      </Typography>
+    </div>
+
+    <Typography variant="inherit" style={styles.error}>
+      {message}
+    </Typography>
+  </FormControl>
 );
 
 export default StyledCheckbox;
