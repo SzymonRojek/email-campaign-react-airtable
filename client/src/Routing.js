8e6e2a3a65dcf7f9d672b51257d365c0e1424a5e
Szymon Rojek <sz.rojek@gmail.com>
2022-01-29 23:02:38 +0000

    create Routing component

---
+++ client/src/Routing.js
@@ -0,0 +1,137 @@
+import { useRoutes } from "react-router-dom";
+
+import "App.css";
+import { SubNavigation } from "components/Navigation";
+import { HomePage } from "pages/homePage";
+import {
+  SubscribersPage,
+  SubscribersStatusPage,
+  AddSubscriberPage,
+  SubscriberDetailsPage,
+} from "pages/subscribers";
+import {
+  CampaignEditPage,
+  AddCampaignPage,
+  CampaignsPage,
+  CampaignsStatusPage,
+} from "pages/campaigns";
+import {
+  subscribersLinksNavigation,
+  campaignsLinksNavigation,
+} from "data/dataLinksNavigation";
+import { NotFoundPage } from "pages/notFoundPage";
+import EditSubscriberPage from "./pages/subscribers/EditSubscriberPage";
+
+const Routing = (props) => {
+  const {
+    subscribersData,
+    handleEditSubscriber,
+    handleSubscriberDetails,
+    handleRemoveItem,
+    getSubscribersData,
+    campaignsData,
+    handleEditCampaign,
+    getCampaignsData,
+  } = props;
+
+  const routes = [
+    { path: "/", element: <HomePage /> },
+    {
+      path: "subscribers",
+      element: <SubNavigation dataLinks={subscribersLinksNavigation} />,
+      children: [
+        {
+          path: "/",
+          element: (
+            <SubscribersPage
+              subscribersData={subscribersData}
+              editSubscriber={handleEditSubscriber}
+              handleSubscriberDetails={handleSubscriberDetails}
+              removeSubscriber={handleRemoveItem}
+            />
+          ),
+        },
+        {
+          path: "status",
+          element: (
+            <SubscribersStatusPage
+              subscribersData={subscribersData}
+              editSubscriber={handleEditSubscriber}
+              handleSubscriberDetails={handleSubscriberDetails}
+              removeSubscriber={handleRemoveItem}
+            />
+          ),
+        },
+        {
+          path: "add",
+          element: (
+            <AddSubscriberPage
+              subscribersData={subscribersData}
+              getSubscribersData={getSubscribersData}
+            />
+          ),
+        },
+        {
+          path: "/details/:id",
+          element: <SubscriberDetailsPage />,
+        },
+        {
+          path: "/edit/:id",
+          element: <EditSubscriberPage />,
+        },
+        { path: "*", element: <NotFoundPage /> },
+      ],
+    },
+    {
+      path: "campaigns",
+      element: <SubNavigation dataLinks={campaignsLinksNavigation} />,
+      children: [
+        {
+          path: "/",
+          element: (
+            <CampaignsPage
+              campaignsData={campaignsData}
+              editCampaign={handleEditCampaign}
+              removeCampaign={handleRemoveItem}
+            />
+          ),
+        },
+        {
+          path: "status",
+          element: (
+            <CampaignsStatusPage
+              campaignsData={campaignsData}
+              editCampaign={handleEditCampaign}
+              removeCampaign={handleRemoveItem}
+            />
+          ),
+        },
+        {
+          path: "add",
+          element: (
+            <AddCampaignPage
+              subscribersData={subscribersData}
+              getCampaignsData={getCampaignsData}
+            />
+          ),
+        },
+        {
+          path: "/edit/:id",
+          element: (
+            <CampaignEditPage
+              subscribersData={subscribersData}
+              getCampaignsData={getCampaignsData}
+            />
+          ),
+        },
+      ],
+    },
+    { path: "*", element: <NotFoundPage /> },
+  ];
+
+  const routing = useRoutes(routes);
+
+  return <div className="routing-container">{routing}</div>;
+};
+
+export default Routing;
