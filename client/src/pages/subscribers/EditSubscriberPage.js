57737fef631f932ffd3416326d17129a9a6fd72b
Szymon Rojek <sz.rojek@gmail.com>
2022-01-29 23:09:06 +0000

    create EditSubscriberPage component

---
+++ client/src/pages/subscribers/EditSubscriberPage.js
@@ -0,0 +1,15 @@
+const EditSubscriberPage = () => (
+  <p
+    style={{
+      padding: 20,
+      backgroundColor: "green",
+      color: "white",
+      fontWeight: "bold",
+      marginTop: 100,
+    }}
+  >
+    here will be edit subscriber page content
+  </p>
+);
+
+export default EditSubscriberPage;
--- client/src/pages/subscribers/index.js
+++ client/src/pages/subscribers/index.js
@@ -2,3 +2,4 @@ export { default as SubscribersPage } from "./SubscribersPage";
 export { default as SubscribersStatusPage } from "./SubscribersStatusPage";
 export { default as SubscriberDetailsPage } from "./SubscriberDetailsPage";
 export { default as AddSubscriberPage } from "./AddSubscriberPage";
+export { default as EditSubscriberPage } from "./EditSubscriberPage";
