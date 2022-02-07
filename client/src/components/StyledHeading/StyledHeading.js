ae92f2567cff4e67da83ec84385a6c5f3e596acd
Szymon Rojek <sz.rojek@gmail.com>
2022-02-07 23:57:20 +0000

    add breakpoints, makeStyles function

--- client/src/components/StyledHeading/StyledHeading.js
+++ client/src/components/StyledHeading/StyledHeading.js
@@ -1,17 +1,31 @@
 import PropTypes from "prop-types";
+import { makeStyles } from "@material-ui/core/styles";
 
-const headingStyles = {
-  marginBottom: 40,
-  paddingTop: 35,
-  color: "#142F43",
-  letterSpacing: 2,
-  wordSpacing: 15,
-  textAlign: "center",
-  textTransform: "uppercase",
-  textShadow: "-2px 2px white",
+const useStyles = makeStyles((theme) => ({
+  heading: {
+    marginTop: 150,
+    marginBottom: 30,
+    color: "white",
+    fontSize: 40,
+    letterSpacing: 2,
+    wordSpacing: 15,
+    textAlign: "center",
+    textTransform: "uppercase",
+    [theme.breakpoints.down("sm")]: {
+      fontSize: 20,
+    },
+    [theme.breakpoints.up("md")]: {
+      marginTop: 280,
+      fontSize: 30,
+    },
+  },
+}));
+
+const StyledHeading = ({ label }) => {
+  const classes = useStyles();
+
+  return <h1 className={classes.heading}>{label}</h1>;
 };
-
-const StyledHeading = ({ label }) => <h1 style={headingStyles}>{label}</h1>;
 
 StyledHeading.propTypes = {
   label: PropTypes.string,
