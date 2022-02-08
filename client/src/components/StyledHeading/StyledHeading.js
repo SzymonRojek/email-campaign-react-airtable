7588fa3b6600461f80ffee60407abf3078823969
Szymon Rojek <sz.rojek@gmail.com>
2022-02-08 17:45:31 +0000

    change heading tyles

--- client/src/components/StyledHeading/StyledHeading.js
+++ client/src/components/StyledHeading/StyledHeading.js
@@ -3,20 +3,15 @@ import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
   heading: {
-    marginTop: 150,
-    marginBottom: 30,
+    marginBottom: 50,
+    fontSize: 30,
     color: "white",
-    fontSize: 40,
     letterSpacing: 2,
     wordSpacing: 15,
     textAlign: "center",
     textTransform: "uppercase",
-    [theme.breakpoints.down("sm")]: {
-      fontSize: 20,
-    },
     [theme.breakpoints.up("md")]: {
-      marginTop: 280,
-      fontSize: 30,
+      fontSize: 50,
     },
   },
 }));
