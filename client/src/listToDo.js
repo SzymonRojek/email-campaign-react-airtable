75a2e3b652890fca492b5cfc1cfdedaf8e94e94e
Szymon Rojek <sz.rojek@gmail.com>
2022-02-10 08:20:36 +0000

    remove

--- client/src/listToDo.js
+++
@@ -1,5 +0,0 @@
-// preapre readme, igo img
-// error change props - at the moment is titleOne etc
-// edit select => add Divider etc
-// style img background
-// probably navLink rv 6 maybe change active - SIMPLIFY
