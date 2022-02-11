e15cf50d03572f479bf83efc8562aedecbdc82d1
Szymon Rojek <sz.rojek@gmail.com>
2022-02-11 19:22:52 +0000

    edit styles

--- client/src/components/DisplayMessage/Loader/Loader.js
+++ client/src/components/DisplayMessage/Loader/Loader.js
@@ -3,15 +3,13 @@ import PropTypes from "prop-types";
 import "./styles.css";
 
 const Loader = ({ title }) => (
-  <div className="main-container">
-    <div className="loader-container">
-      <div className="load"></div>
+  <div className="loader-container">
+    <div className="load"></div>
 
-      <div className="text-container">
-        <div className="dot-container">
-          <p className="loader-text">{title}</p>
-          <div className="dot-flashing"></div>
-        </div>
+    <div className="text-container">
+      <div className="dot-container">
+        <p className="loader-text">{title}</p>
+        <div className="dot-flashing"></div>
       </div>
     </div>
   </div>
