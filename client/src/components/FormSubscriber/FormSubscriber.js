1c302360804ef3a234ee41f207052e730805099b
Szymon Rojek <sz.rojek@gmail.com>
2022-02-11 12:24:29 +0000

    edit styles, change some props

--- client/src/components/FormSubscriber/FormSubscriber.js
+++ client/src/components/FormSubscriber/FormSubscriber.js
@@ -2,6 +2,7 @@ import PropTypes from "prop-types";
 import { Paper, Box, Collapse, Button } from "@mui/material";
 import { Grid, Typography } from "@material-ui/core";
 import { ExpandLess, ExpandMore } from "@material-ui/icons";
+import { makeStyles } from "@material-ui/core/styles";
 
 import {
   TextInputController,
@@ -16,6 +17,59 @@ const selectStatusData = [
   { value: "active", label: "active" },
 ];
 
+const useSelectStyles = makeStyles({
+  root: {
+    "& .MuiOutlinedInput-input": {
+      color: "rgb(221, 220, 220)",
+    },
+
+    "& .MuiInputLabel-root": {
+      color: "rgb(221, 220, 220)",
+    },
+    "& .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline": {
+      borderColor: "rgb(221, 220, 220)",
+    },
+    "& .MuiSvgIcon-root": {
+      color: "rgb(221, 220, 220)",
+    },
+    "&:hover .MuiOutlinedInput-input": {
+      color: "rgb(221, 220, 220)",
+    },
+    "&:hover .MuiInputLabel-root": {
+      color: "rgb(221, 220, 220)",
+    },
+    "&:hover .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline": {
+      borderColor: "rgb(221, 220, 220)",
+    },
+    "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-input": {
+      color: "rgb(221, 220, 220)",
+    },
+    "& .MuiInputLabel-root.Mui-focused": {
+      color: "rgb(221, 220, 220)",
+    },
+    "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline": {
+      borderColor: "#ffa500",
+    },
+  },
+});
+
+const useButtonStyles = makeStyles((theme) => ({
+  root: {
+    "&.MuiButton-root.MuiButton-contained": {
+      backgroundColor: "orange",
+      border: 0,
+      color: "#142f43",
+      fontWeight: "bold",
+      letterSpacing: 1,
+      padding: "17px 15px",
+      [theme.breakpoints.down("sm")]: {
+        fontSize: 12,
+        padding: "12px 10px",
+      },
+    },
+  },
+}));
+
 const styles = {
   paper: {
     maxWidth: 600,
@@ -25,6 +79,7 @@ const styles = {
     backgroundColor: "#142F43",
   },
   typographyRequiredText: { color: "orange", letterSpacing: 2, wordSpacing: 3 },
+  textError: { color: "crimson", paddingTop: 10 },
   icon: { color: "orange", fontSize: 30, marginTop: 6 },
 };
 
@@ -33,133 +88,148 @@ const FormSubscriber = ({
   errors,
   handleSubmit,
   isCheckboxChecked,
-}) => (
-  <Paper elevation={14} style={styles.paper}>
-    <form onSubmit={handleSubmit}>
-      <Grid container spacing={4}>
-        <Grid item xs={12}>
-          <Typography
-            color="textSecondary"
-            variant="body2"
-            style={styles.typographyRequiredText}
-          >
-            *Fields required
-          </Typography>
-        </Grid>
-        <Grid item xs={12}>
-          <TextInputController
-            control={control}
-            name="name"
-            label="name"
-            defaultValue=""
-            error={!!errors.name}
-            message={errors.name?.message ?? ""}
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <TextInputController
-            control={control}
-            name="surname"
-            label="surname"
-            defaultValue=""
-            error={!!errors.surname}
-            message={errors.surname?.message ?? ""}
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <TextInputController
-            control={control}
-            name="email"
-            label="email"
-            defaultValue=""
-            error={!!errors.email}
-            message={errors.email?.message ?? ""}
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <SelectInputController
-            control={control}
-            name="status"
-            defaultValue="select status"
-            error={!!errors.status}
-            message={errors.status?.message ?? ""}
-            data={selectStatusData}
-            styles={styles.select}
-          />
-        </Grid>
-        <Grid container item xs={12}>
-          <Grid item xs={11}>
-            <CheckboxInputController
+}) => {
+  const classesSelectStyles = useSelectStyles();
+  const classesButtonStyles = useButtonStyles();
+
+  return (
+    <Paper elevation={14} style={styles.paper}>
+      <form onSubmit={handleSubmit}>
+        <Grid container spacing={4}>
+          <Grid item xs={12}>
+            <Typography
+              color="textSecondary"
+              variant="body2"
+              style={styles.typographyRequiredText}
+            >
+              *fields required
+            </Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <TextInputController
               control={control}
-              name="checkbox"
-              defaultValue={false}
-              error={!!errors.checkbox}
-              message={errors.checkbox?.message ?? ""}
-              label={
-                isCheckboxChecked ? "Close more details" : "Open more details"
-              }
+              name="name"
+              label="name"
+              defaultValue=""
+              error={!!errors.name}
+              message={errors.name?.message ?? ""}
             />
           </Grid>
-          <Grid item xs={1}>
-            {isCheckboxChecked ? (
-              <ExpandLess style={styles.icon} />
-            ) : (
-              <ExpandMore style={styles.icon} />
-            )}
+          <Grid item xs={12}>
+            <TextInputController
+              control={control}
+              name="surname"
+              label="surname"
+              defaultValue=""
+              error={!!errors.surname}
+              message={errors.surname?.message ?? ""}
+            />
           </Grid>
-        </Grid>
-
-        <Collapse in={isCheckboxChecked} timeout="auto" unmountOnExit>
-          <Box px={2} py={3}>
-            <Grid container spacing={4}>
-              <Grid item xs={12}>
-                <TextInputController
-                  control={control}
-                  name="profession"
-                  label="profession"
-                  defaultValue=""
-                  error={!!errors.profession}
-                  message={errors.profession?.message ?? ""}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextInputController
-                  control={control}
-                  name="salary"
-                  label="salary"
-                  defaultValue=""
-                  error={!!errors.salary}
-                  message={errors.salary?.message ?? ""}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextInputController
-                  control={control}
-                  name="telephone"
-                  label="telephone +44"
-                  defaultValue=""
-                  error={!!errors.telephone}
-                  message={errors.telephone?.message ?? ""}
-                />
-              </Grid>
+          <Grid item xs={12}>
+            <TextInputController
+              control={control}
+              name="email"
+              label="email"
+              defaultValue=""
+              error={!!errors.email}
+              message={errors.email?.message ?? ""}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <SelectInputController
+              control={control}
+              name="status"
+              defaultValue="select status"
+              error={!!errors.status}
+              message={errors.status?.message ?? ""}
+              data={selectStatusData}
+              classesSelectStyles={classesSelectStyles.root}
+              styles={styles.textError}
+            />
+          </Grid>
+          <Grid container item xs={12}>
+            <Grid item xs={11}>
+              <CheckboxInputController
+                control={control}
+                name="checkbox"
+                defaultValue={false}
+                error={!!errors.checkbox}
+                message={errors.checkbox?.message ?? ""}
+                label={
+                  isCheckboxChecked ? "Close more details" : "Open more details"
+                }
+              />
             </Grid>
-          </Box>
-        </Collapse>
+            <Grid item xs={1}>
+              {isCheckboxChecked ? (
+                <ExpandLess style={styles.icon} />
+              ) : (
+                <ExpandMore style={styles.icon} />
+              )}
+            </Grid>
+          </Grid>
 
-        <Grid
-          container
-          direction="row"
-          justifyContent="space-evenly"
-          alignItems="center"
-        >
-          <Grid item>
-            <Button label="submit" ariaLabel="add subscriber" type="submit" />
+          <Collapse in={isCheckboxChecked} timeout="auto" unmountOnExit>
+            <Box px={2} py={3}>
+              <Grid container spacing={4}>
+                <Grid item xs={12}>
+                  <TextInputController
+                    control={control}
+                    name="profession"
+                    label="profession"
+                    defaultValue=""
+                    error={!!errors.profession}
+                    message={errors.profession?.message ?? ""}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextInputController
+                    control={control}
+                    name="salary"
+                    label="salary"
+                    defaultValue=""
+                    error={!!errors.salary}
+                    message={errors.salary?.message ?? ""}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextInputController
+                    control={control}
+                    name="telephone"
+                    label="telephone +44"
+                    defaultValue=""
+                    error={!!errors.telephone}
+                    message={errors.telephone?.message ?? ""}
+                  />
+                </Grid>
+              </Grid>
+            </Box>
+          </Collapse>
+
+          <Grid
+            container
+            direction="row"
+            justifyContent="space-evenly"
+            alignItems="center"
+          >
+            <Grid item>
+              <Box p={2}>
+                <Button
+                  aria-label="add"
+                  variant="contained"
+                  className={classesButtonStyles.root}
+                  onClick={() => handleSubmit()}
+                >
+                  add subscriber
+                </Button>
+              </Box>
+            </Grid>
           </Grid>
         </Grid>
-      </Grid>
-    </form>
-  </Paper>
-);
+      </form>
+    </Paper>
+  );
+};
 
 FormSubscriber.propTypes = {
   control: PropTypes.object,
