5e2a642cfb97190dffccb13d8fa2b4eb49379b3f
Szymon Rojek <sz.rojek@gmail.com>
2022-02-12 22:43:55 +0000

    edit logout button

--- client/src/components/Navigation/MainNavigation/MainNavigation.js
+++ client/src/components/Navigation/MainNavigation/MainNavigation.js
@@ -79,16 +79,29 @@ const useStyles = makeStyles((theme) => ({
     },
   },
   logOutButton: {
-    maxWidth: 120,
-    padding: "15px 20px",
-    fontSize: 16,
-    fontWeight: "bold",
-    color: "#142f43",
-    backgroundColor: "orange",
-    textTransform: "uppercase",
-    border: "none",
-    borderRadius: 3,
-    cursor: "pointer",
+    "&.MuiButton-root": {
+      maxWidth: 120,
+      padding: "10px 20px",
+      fontSize: 16,
+      fontWeight: "bold",
+      color: "#142f43",
+      backgroundColor: "orange",
+      textTransform: "uppercase",
+      border: "none",
+      borderRadius: 3,
+      cursor: "pointer",
+      transition: "all .3s ease",
+
+      "&:hover": {
+        backgroundColor: "#ca880e",
+        color: "rgb(221, 220, 220)",
+        transform: "translateY(-2px)",
+      },
+
+      "&:active": {
+        transform: "translateY(2px)",
+      },
+    },
   },
 }));
 
