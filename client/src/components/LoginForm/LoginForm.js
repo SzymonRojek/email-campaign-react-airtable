ae77de0122d5cc1e7ce4af4659da9102da18fc56
Szymon Rojek <sz.rojek@gmail.com>
2022-02-12 22:44:14 +0000

    edit log in button

--- client/src/components/LoginForm/LoginForm.js
+++ client/src/components/LoginForm/LoginForm.js
@@ -28,17 +28,30 @@ const styles = {
 
 const useStyles = makeStyles((theme) => ({
   logInButton: {
-    margin: "50px auto",
-    maxWidth: 120,
-    padding: "20px 25px",
-    fontSize: 16,
-    fontWeight: "bold",
-    color: "#142f43",
-    backgroundColor: "orange",
-    textTransform: "uppercase",
-    border: "none",
-    borderRadius: 3,
-    cursor: "pointer",
+    "&.MuiButton-root": {
+      maxWidth: 120,
+      margin: "20px 0 20px 0",
+      padding: "10px 20px",
+      fontSize: 16,
+      fontWeight: "bold",
+      color: "#142f43",
+      backgroundColor: "orange",
+      textTransform: "uppercase",
+      border: "none",
+      borderRadius: 3,
+      cursor: "pointer",
+      transition: "all .3s ease",
+
+      "&:hover": {
+        backgroundColor: "#ca880e",
+        color: "rgb(221, 220, 220)",
+        transform: "translateY(-2px)",
+      },
+
+      "&:active": {
+        transform: "translateY(2px)",
+      },
+    },
   },
 }));
 
