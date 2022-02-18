7a9ccce02d6274d35626c7bb62afa76f255a033b
Szymon Rojek <sz.rojek@gmail.com>
2022-02-18 15:51:09 +0000

    remove app, remove script

--- server/server.js
+++ server/server.js
@@ -1,4 +1,19 @@
-const app = require("./app");
+const express = require("express");
+const cors = require("cors");
+const subscribersRouter = require("./routes/subscribersRoutes");
+const campaignsRouter = require("./routes/campaignsRoutes");
+
+require("dotenv").config();
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/subscribers", subscribersRouter);
+app.use("/campaigns", campaignsRouter);
 
 const PORT = process.env.PORT || 5000;
 
