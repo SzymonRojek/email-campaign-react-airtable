89a8be320874d0b14a611777d7947bf2867303d8
Szymon Rojek <sz.rojek@gmail.com>
2022-02-18 18:02:22 +0000

    import path

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const cors = require("cors");
 const subscribersRouter = require("./routes/subscribersRoutes");
 const campaignsRouter = require("./routes/campaignsRoutes");
