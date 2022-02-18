9a0e0bbcb7488264e485839d3ad7c5bc6c4dce62
Szymon Rojek <sz.rojek@gmail.com>
2022-02-18 15:21:16 +0000

    remove StyledMainContainer, add marginTop to the css

--- client/src/components/DisplayMessage/Error/Error.js
+++ client/src/components/DisplayMessage/Error/Error.js
@@ -1,16 +1,13 @@
 import PropTypes from "prop-types";
 
 import "./styles.css";
-import { StyledMainContent } from "../../StyledMainContent";
 
 const Error = ({ titleOne, titleTwo, titleThree }) => (
-  <StyledMainContent>
-    <div className="error-content">
-      <p className="error-text error-mainText">{titleOne}</p>
-      <p className="error-text error-subText">{titleTwo}</p>
-      <p className="error-text error-subText">{titleThree}</p>
-    </div>
-  </StyledMainContent>
+  <div className="error-content">
+    <p className="error-text error-mainText"></p>
+    <p className="error-text error-subText">{titleTwo}</p>
+    <p className="error-text error-subText">{titleThree}</p>
+  </div>
 );
 
 Error.propTypes = {
