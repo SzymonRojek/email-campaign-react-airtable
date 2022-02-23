d737f035d9caf6b22bf375a99b7ab5600b1928fd
Szymon Rojek <sz.rojek@gmail.com>
2022-02-23 01:39:01 +0000

    rename popupContextProvider to GlobalStoreContextProvider

---
+++ client/src/contexts/GlobalStoreContextProvider.js
@@ -0,0 +1,45 @@
+import React, { createContext, useState, useContext } from "react";
+import { useLocalStorageValue } from "customHooks/useLocalStorageValue";
+
+const GlobalStoreContext = createContext();
+
+export const GlobalStoreContextProvider = ({ children }) => {
+  // login form
+  const [statusLog, setStatusLog] = useLocalStorageValue("status", "loadingIn");
+  const [isLogIn, setIsLogIn] = useLocalStorageValue("login", false);
+
+  // navigation tabs
+  const [tabsValue, setTabsValue] = useState(0);
+  const [tabsSubValue, setTabsSubValue] = useState(0);
+
+  // selectedActiveSubscribers in the popup
+  const [finalSelectedActiveSubscribers, setFinalSelectedActiveSubscribers] =
+    useState(null);
+
+  const contextValues = {
+    statusLog,
+    setStatusLog,
+    isLogIn,
+    setIsLogIn,
+    tabsValue,
+    setTabsValue,
+    tabsSubValue,
+    setTabsSubValue,
+    finalSelectedActiveSubscribers,
+    setFinalSelectedActiveSubscribers,
+  };
+
+  return (
+    <GlobalStoreContext.Provider value={contextValues}>
+      {children}
+    </GlobalStoreContext.Provider>
+  );
+};
+
+export const useGlobalStoreContext = () => {
+  const context = useContext(GlobalStoreContext);
+  if (context === undefined) {
+    throw new Error("GlobalStoreContext must be used within a Provider");
+  }
+  return context;
+};
