6dbbd737583267fc52a32e93ce4b05a3f9252768
Szymon Rojek <sz.rojek@gmail.com>
2022-02-23 01:43:56 +0000

    create index file with exports

---
+++ client/src/contexts/index.js
@@ -0,0 +1,4 @@
+export { default as APIcontextProvider } from "./APIcontextProvider";
+export { default as ConfirmModalContext } from "./ConfirmModalContext";
+export { default as GlobalStoreContextProvider } from "./GlobalStoreContextProvider";
+export { default as InformationModalContext } from "./InformationModalContext";
