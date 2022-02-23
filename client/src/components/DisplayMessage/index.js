f54f5135a200a8d7e54b27720cec1f5263991b4a
Szymon Rojek <sz.rojek@gmail.com>
2022-02-23 01:41:54 +0000

    edit exports

--- client/src/components/DisplayMessage/index.js
+++ client/src/components/DisplayMessage/index.js
@@ -1,4 +1,2 @@
 export { default as Error } from "./Error";
 export { default as Loader } from "./Loader";
-export { default as InfoPopup } from "./InfoPopup";
-export { default as ConfirmPopup } from "./ConfirmPopup";
