96fffeee8ae70c3e249f5af9a471e1bb8c5ad3d6
Szymon Rojek <sz.rojek@gmail.com>
2022-03-06 23:11:52 +0000

    add functions fetchDetailsItemById and fetchItems, export them

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,20 +1,20 @@
 import axios from "axios";
 
-const request = async (endpoint, method = "GET", data = null) => {
+const request = async (endpoint = "", method = "GET", data = {}) => {
   const requestConfig = {
     method,
     url: endpoint,
     headers: {
       "Content-type": "application/json",
     },
-    data: method === "POST" || method === "PATCH" ? data : null,
+    data: method === "POST" || method === "PATCH" ? data : {},
   };
 
   const response = await axios(requestConfig);
 
-  // if (response.statusText !== "OK") {
-  //   throw new Error("Something happened - no data");
-  // }
+  if (!response) {
+    throw new Error("Something happened - no data");
+  }
 
   return response.data;
 };
@@ -25,10 +25,26 @@ const post = (endpoint, data) => request(endpoint, "POST", data);
 
 const patch = (endpoint, data) => request(endpoint, "PATCH", data);
 
-const _delete = (endpoint) => request(endpoint, "DELETE");
+const _delete = (endpoint) => request(endpoint, "delete");
+
+const fetchDetailsItemById = async ({ queryKey }) => {
+  const [key, { id }] = queryKey;
+  const response = await get(`/${key}/${id}`);
+
+  return response;
+};
+
+const fetchItems = async ({ queryKey }) => {
+  const [key] = queryKey;
+  const response = await get(`${key}`);
+
+  return response;
+};
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
+  fetchDetailsItemById,
+  fetchItems,
   get,
   post,
   patch,
