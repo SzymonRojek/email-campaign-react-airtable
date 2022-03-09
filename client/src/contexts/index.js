894b546b9ac39f494f27960674d4950cb6218db1
Szymon Rojek <sz.rojek@gmail.com>
2022-03-09 15:23:57 +0000

    edit exports

--- client/src/contexts/index.js
+++ client/src/contexts/index.js
@@ -1,4 +1,1 @@
-export { default as APIcontextProvider } from "./APIcontextProvider";
-export { default as ConfirmModalContext } from "./ConfirmModalContext";
-export { default as GlobalStoreContextProvider } from "./GlobalStoreContextProvider";
-export { default as InformationModalContext } from "./InformationModalContext";
+export * from "contexts";
