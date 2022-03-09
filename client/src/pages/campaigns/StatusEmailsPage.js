1e22ab235ea96b42ba648104d254f91cae15e0f5
Szymon Rojek <sz.rojek@gmail.com>
2022-03-09 15:35:07 +0000

    remove useAPIcontext, add useQuery functionality and fetch campaigns, edit component on return

--- client/src/pages/campaigns/StatusEmailsPage.js
+++ client/src/pages/campaigns/StatusEmailsPage.js
@@ -1,10 +1,11 @@
 import PropTypes from "prop-types";
 import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
+import { useQuery } from "react-query";
 import { makeStyles } from "@material-ui/core/styles";
 
-import { useAPIcontext } from "contexts/APIcontextProvider";
-import { Loader, Error } from "components/DisplayMessage";
+import api from "api";
+import { Loader } from "components/DisplayMessage";
 import { StyledContainer } from "components/StyledContainer";
 import { StyledMainContent } from "components/StyledMainContent";
 import { StyledHeading } from "components/StyledHeading";
@@ -62,11 +63,15 @@ const selectCampaignsStatus = [
   { value: "draft", label: "draft" },
 ];
 
-const StatusEmailsPage = ({ editCampaign, removeCampaign }) => {
-  const { campaignsData } = useAPIcontext();
+const StatusEmailsPage = ({ editCampaign }) => {
+  const { data: campaigns, isLoading } = useQuery("campaigns", api.fetchItems, {
+    meta: {
+      myMessage: "Can not get campaigns list:",
+    },
+  });
+
   const { control, watch } = useForm();
   const [selectStatus, setSelectStatus] = useState("sent");
-
   const classesSelectStyles = useSelectStyles();
 
   const statusDataHeadTable = [
@@ -95,60 +100,32 @@ const StatusEmailsPage = ({ editCampaign, removeCampaign }) => {
     return () => watchStatus.unsubscribe();
   }, [watch]);
 
+  if (isLoading) {
+    return <Loader title="Status" />;
+  }
+
   return (
-    <>
-      {campaignsData.status === "loading" ? (
-        <Loader title="Status" />
-      ) : campaignsData.status === "error" ? (
-        <Error
-          titleOne="ERROR MESSAGE"
-          titleTwo="Probably there is no an access to the internet."
-          titleThree="Contact with your internet provider."
-        />
-      ) : !campaignsData.data.length ? (
-        <Error
-          titleOne="MESSAGE"
-          titleTwo="There are no Emails added yet"
-          titleThree="Please add a New Email"
-        />
-      ) : (
-        campaignsData.status === "success" && (
-          <StyledContainer>
-            <StyledHeading label="email status" />
-            <StyledMainContent>
-              <CampaignStatus
-                subHeading="list"
-                dataHeadEmailTable={statusDataHeadTable}
-                passedData={campaignsData.data}
-                status={selectStatus}
-                editCampaign={editCampaign}
-                removeCampaign={removeCampaign}
-              />
-            </StyledMainContent>
-          </StyledContainer>
-        )
-      )}
-    </>
+    <StyledContainer>
+      <StyledHeading label="email status" />
+      <StyledMainContent>
+        {campaigns && !campaigns.length ? (
+          "List of campaigns is empty - please add email"
+        ) : (
+          <CampaignStatus
+            subHeading="list"
+            dataHeadEmailTable={statusDataHeadTable}
+            passedData={campaigns}
+            status={selectStatus}
+            editCampaign={editCampaign}
+          />
+        )}
+      </StyledMainContent>
+    </StyledContainer>
   );
 };
 
 StatusEmailsPage.propTypes = {
-  campaignsData: PropTypes.shape({
-    status: PropTypes.string,
-    data: PropTypes.arrayOf(
-      PropTypes.shape({
-        id: PropTypes.string,
-        createdTime: PropTypes.string,
-        fields: PropTypes.shape({
-          status: PropTypes.string,
-          title: PropTypes.string,
-          description: PropTypes.string,
-        }),
-      })
-    ),
-  }),
   editCampaign: PropTypes.func,
-  removeCampaign: PropTypes.func,
 };
 
 export default StatusEmailsPage;
