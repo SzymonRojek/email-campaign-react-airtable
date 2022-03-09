6494dcccbd407287a0335cced60b4ecf696d2d17
Szymon Rojek <sz.rojek@gmail.com>
2022-03-09 15:22:22 +0000

    remove useAPIcontext, remover handleRemoveItem, import Modals

--- client/src/AppContainer.js
+++ client/src/AppContainer.js
@@ -1,43 +1,15 @@
 import { useNavigate } from "react-router-dom";
 
 import "App.css";
-import api from "api";
-import { useAPIcontext } from "contexts/APIcontextProvider";
 import Routing from "./Routing";
 import { MainNavigation } from "components/Navigation";
 import { Login } from "./Login";
 import { StyledFooter } from "components/StyledFooter";
 import { GlobalStoreContextProvider } from "contexts/GlobalStoreContextProvider";
+import Modals from "./Modals";
 
 export const AppContainer = () => {
   const navigate = useNavigate();
-
-  const {
-    subscribersData,
-    setSubscribersData,
-    campaignsData,
-    setCampaignsData,
-  } = useAPIcontext();
-
-  const handleRemoveItem = (id, endpoint) => {
-    api.delete(`/${endpoint}/${id}`);
-
-    const filteredGroup = (group) => group.filter((item) => item.id !== id);
-
-    if (endpoint === "subscribers") {
-      setSubscribersData({
-        status: "success",
-        data: filteredGroup(subscribersData.data),
-      });
-    }
-
-    if (endpoint === "campaigns") {
-      setCampaignsData({
-        status: "success",
-        data: filteredGroup(campaignsData.data),
-      });
-    }
-  };
 
   const handleSubscriberDetails = (subscriber) => {
     if (subscriber.fields.status === "active")
@@ -54,19 +26,20 @@ export const AppContainer = () => {
   };
 
   return (
-    <GlobalStoreContextProvider>
-      <MainNavigation />
+    <Modals>
+      <GlobalStoreContextProvider>
+        <MainNavigation />
 
-      <Login>
-        <Routing
-          handleEditSubscriber={handleEditSubscriber}
-          handleSubscriberDetails={handleSubscriberDetails}
-          handleEditCampaign={handleEditCampaign}
-          handleRemoveItem={handleRemoveItem}
-        />
-      </Login>
+        <Login>
+          <Routing
+            handleEditSubscriber={handleEditSubscriber}
+            handleSubscriberDetails={handleSubscriberDetails}
+            handleEditCampaign={handleEditCampaign}
+          />
+        </Login>
 
-      <StyledFooter label="Coded By Szymon Rojek © 2022" />
-    </GlobalStoreContextProvider>
+        <StyledFooter label="Coded By Szymon Rojek © 2022" />
+      </GlobalStoreContextProvider>
+    </Modals>
   );
 };
