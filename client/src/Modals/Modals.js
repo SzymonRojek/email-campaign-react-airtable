492d73a556a0f9dad9e8d83d74ec9554c0343a1c
Szymon Rojek <sz.rojek@gmail.com>
2022-06-05 22:16:17 +0100

    remove fragment

--- client/src/Modals/Modals.js
+++ client/src/Modals/Modals.js
@@ -2,11 +2,9 @@ import { ConfirmModalContext } from "../contexts/ConfirmModalContext";
 import { InformationModalContext } from "../contexts/InformationModalContext";
 
 const Modals = ({ children }) => (
-  <>
-    <ConfirmModalContext>
-      <InformationModalContext>{children}</InformationModalContext>
-    </ConfirmModalContext>
-  </>
+  <ConfirmModalContext>
+    <InformationModalContext>{children}</InformationModalContext>
+  </ConfirmModalContext>
 );
 
 export default Modals;
