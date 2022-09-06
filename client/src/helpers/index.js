898397bf08732373ec0e7221be0a58008553830e
Szymon Rojek <sz.rojek@gmail.com>
2022-09-06 22:11:16 +0100

    remove two exports

--- client/src/helpers/index.js
+++ client/src/helpers/index.js
@@ -3,8 +3,6 @@ export { default as formattedData } from "./formattedData";
 export { default as isEven } from "./isEven";
 export { default as validationSubscriber } from "./validationSubscriber";
 export { default as validationCampaign } from "./validationCampaign";
-export { default as sortDataAlphabetically } from "./sortDataAlphabetically";
-export { default as capitalizeFirstLetter } from "./capitalizeFirstLetter";
 export { default as getLatestAddedItem } from "./getLatestAddedItem";
 export { default as getFilteredDataByStatus } from "./getFilteredDataByStatus";
 export { default as validationLogin } from "./validationLogin";
