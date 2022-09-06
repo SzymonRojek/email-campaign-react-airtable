d714adf545929f20d8edf8fde23c9536684232d9
Szymon Rojek <sz.rojek@gmail.com>
2022-09-06 22:10:36 +0100

    remove seconds

--- client/src/helpers/formattedData.js
+++ client/src/helpers/formattedData.js
@@ -2,7 +2,7 @@ import moment from "moment";
 
 const getFormattedDate = (date) => moment(date).format("YYYY/MM/DD");
 
-const getFormattedTime = (time) => moment(time).format("h:mm:ss a");
+const getFormattedTime = (time) => moment(time).format("h:mm a");
 
 const formattedData = { getFormattedDate, getFormattedTime };
 
